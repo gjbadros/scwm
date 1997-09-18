5fbaedc8f9edee3d2caa63ea230ee12346b10768
Maciej Stachowiak <mstachow@alum.mit.edu>
1997-09-18 05:26:44 +0000

    Added set-pixmap-path! and set-icon-path! functions.

--- src/paths.c
+++ src/paths.c
@@ -1,42 +1,52 @@
+#include <guile/gh.h>
+#include "../configure.h"
+#include "scwm.h"
+#include "screen.h"
+#include "errors.h"
+#include "util.h"
 
-#if 0
+
 
 #ifdef XPM
 char *PixmapPath = SCWM_ICONDIR;
-void setPixmapPath(XEvent *eventp,Window w,ScwmWindow *tmp_win,
-                   unsigned long context, char *action,int* Module)
+
+SCM set_pixmap_path_x(SCM newpath)
 {
   static char *ptemp = NULL;
   char *tmp;
+  in dummy;
 
   if(ptemp == NULL)
     ptemp = PixmapPath;
-
   if((PixmapPath != ptemp)&&(PixmapPath != NULL))
     free(PixmapPath);
-  tmp = stripcpy(action);
+
+  tmp = gh_scm2newstr(newpath,&dummy);
+
   PixmapPath = envDupExpand(tmp, 0);
   free(tmp);
 }
 #endif
 
 char *IconPath = SCWM_ICONDIR;
-void setIconPath(XEvent *eventp,Window w,ScwmWindow *tmp_win,
-                 unsigned long context, char *action,int* Module)
+
+SCM set_icon_path_x(SCM newpath)
 {
   static char *ptemp = NULL;
   char *tmp;
+  int dummy;
 
   if(ptemp == NULL)
     ptemp = IconPath;
 
   if((IconPath != ptemp)&&(IconPath != NULL))
     free(IconPath);
-  tmp = stripcpy(action);
+  tmp = gh_scm2newstr(newpath,&dummy);
   IconPath = envDupExpand(tmp, 0);
   free(tmp);
 }
 
+#if 0 /* no module support */
 #ifdef SCWM_MODULEDIR
 char *ModulePath = SCWM_MODULEDIR;
 #else
@@ -57,5 +67,7 @@ void setModulePath(XEvent *eventp,Window w,ScwmWindow *tmp_win,
   ModulePath = envDupExpand(tmp, 0);
   free(tmp);
 }
-
 #endif
+
+
+
