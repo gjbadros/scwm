669ace66e15c190560963c1c0efade2aed9347d4
Maciej Stachowiak <mstachow@alum.mit.edu>
1997-09-21 12:11:22 +0000

    New file base.scm - extra basic window manager operations written in Scheme, inclluding %x, %y, execute, set-menu-foreground!, set-menu-background!, set-menu-stipple!, move-pointer, menu-style and title-style.

---
+++ scheme/base.scm
@@ -0,0 +1,60 @@
+;;;; 	Copyright (C) 1997 Maciej Stachowiak
+;;;; 
+;;;; This program is free software; you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; either version 2, or (at your option)
+;;;; any later version.
+;;;; 
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;; 
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this software; see the file COPYING.  If not, write to
+;;;; the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+;;;; Boston, MA 02111-1307 USA
+;;;; 
+
+
+
+(define-module (app scwm base)
+  :use-module (app scwm optargs))
+
+
+
+(define-public (%x x)
+  (inexact->exact (truncate (/ (* x (car (get-display-size))) 100))))
+
+(define-public (%y y)
+  (inexact->exact (truncate (/ (* y (cadr (get-display-size))) 100))))
+
+(define-public (execute command) 
+  (system (string-append "exec " command " &")))
+
+(define-public (set-menu-foreground! fg) (set-menu-colors! fg))
+(define-public (set-menu-background! bg) (set-menu-colors! #f bg))
+(define-public (set-menu-stipple! st) (set-menu-colors! #f #f st))
+
+(define-public (move-pointer x y)
+  (let ((pos (get-pointer-position)))
+    (move-pointer-to (+ x (car pos)) (+ y (cadr pos)))))
+
+(define*-public (menu-style #&key 
+		     (fg #f) (foreground #f)
+		     (bg #f) (background #f)
+		     (stipple #f) font mwm)
+  (set-menu-colors! (or fg foreground) (or bg backgrounf stipple))
+  (if (bound? font)
+      (set-menu-font! font))
+  (if (bound? mwm)
+      (set-menu-mwm-style! mwm)))
+
+(define*-public (title-style #&key font height justify)
+  (if (bound? font)
+      (set-window-font! font))
+  (if (bound? height) 
+      (set-title-height! height))
+  (if (bound? justify)
+      (set-title-justify! justify)))
+
