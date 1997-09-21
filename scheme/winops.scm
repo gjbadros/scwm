298b89bd5f1da3ce4f613864cd7c15dc98fc19e5
Maciej Stachowiak <mstachow@alum.mit.edu>
1997-09-21 12:02:02 +0000

    New file winops.scm - extra window operations defined in Scheme: make-toggling-winop, close-window, toggle-raise, toggle-iconify, toggle-stick, toggle-window-shade, maximize, maximized?, unmaximize, toggle-maximize and print-window.

---
+++ scheme/winops.scm
@@ -0,0 +1,88 @@
+;;;; 	Copyright (C) 1997 Maciej Stachowiak
+;;;; 
+;;;; This program is free software; you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; either version 2, or (at your option)
+;;;; any later version.
+;;;; 
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;; 
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this software; see the file COPYING.  If not, write to
+;;;; the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+;;;; Boston, MA 02111-1307 USA
+;;;; 
+
+
+
+(define-module (app scwm winops)
+  :use-module (app scwm optargs))
+
+
+
+
+
+(define*-public ((make-toggling-winop pred neg pos) 
+		 #&optional (w (get-window)))
+  (if w (if (pred w)
+	    (neg w)
+	    (pos w))))
+
+
+(define-public close-window 
+  (make-toggling-winop window-deletable? delete-window destroy-window))
+
+(define-public toggle-raise
+  (make-toggling-winop raised? lower-window raise-window))
+
+(define-public toggle-iconify
+  (make-toggling-winop iconified? deiconify iconify))
+
+(define-public toggle-stick 
+  (make-toggling-winop sticky? unstick stick))
+
+(define-public toggle-window-shade 
+  (make-toggling-winop window-shaded? un-window-shade window-shade))
+
+(define*-public (maximize nw nh #&optional (w (get-window)))
+  (if w (let* ((pos (get-window-position w))
+	       (size (get-window-size w))
+	       (x (car pos))
+	       (y (cadr pos))
+	       (width (car size))
+	       (height (cadr size)))
+	  (if (not (object-property w 'maximized))
+	      (set-object-property! w 'maximized 
+				    (list x y width height)))
+	  (move-to (if (> nw 0) 0 x)
+		   (if (> nh 0) 0 y) w)
+	  (resize-to (if (> nw 0) nw width)
+		     (if (> nh 0) nh height) w)))
+
+(define*-public (maximized? #&optional (w (get-window)))
+  (->bool (object-property w 'maximized)) #f))
+
+(define*-public (unmaximize #&optional (w (get-window)))
+  (if w (let ((max-prop (object-property w 'maximized)))
+	  (cond
+	   (max-prop (move-to (car max-prop)
+			      (cadr max-prop) w)
+		     (resize-to (caddr max-prop)
+				(cadddr max-prop) w)
+		     (set-object-property! w 'maximized #f))))))
+
+
+(define*-public (toggle-maximize nw nh #&optional (w (get-window)))
+  (if w (if (maximized? w)
+	    (unmaximize w)
+	    (maximize nw nh w))))
+
+(define*-public (print-window #&optional (w (get-window)))
+  (if w (execute (string-append "xwd -id " 
+				(number->string (get-window-id w))
+				" | xpr | lpr"))))
+
+
