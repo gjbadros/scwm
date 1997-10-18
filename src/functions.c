8edb13dd501d11b13103b005e5458ce819fbb20d
Maciej Stachowiak <mstachow@alum.mit.edu>
1997-10-18 01:04:18 +0000

    Commented out some no longer used code with pairs of "#ifdef MS_DELETION_COMMENT" and "#endif /* MS_DELETION_COMMENT */" for easy removal later. Also explicitly undefined MS_DELETION_COMMENT at the top of the file just in case. The function ExecuteFunction and most entries in the func_config array were removed.

--- src/functions.c
+++ src/functions.c
@@ -1,4 +1,3 @@
-
 /****************************************************************************
  * This module is all original code 
  * by Rob Nation 
@@ -28,6 +27,8 @@
 #include "screen.h"
 #include "module.h"
 
+#undef MS_DELETION_COMMENT
+
 extern XEvent Event;
 extern ScwmWindow *Tmp_win;
 extern int menuFromFrameOrWindowOrTitlebar;
@@ -44,17 +45,23 @@ struct functions func_config[] =
 #ifdef USEDECOR
   {"AddToDecor",   add_item_to_decor,F_ADD_DECOR,	    FUNC_NO_WINDOW},
 #endif
+
+#if MS_DELETION_COMMENT
   {"AddToMenu",    add_item_to_menu, F_ADDMENU,             FUNC_NO_WINDOW},
   {"AddToFunc",    add_item_to_func, F_ADDFUNC,             FUNC_NO_WINDOW},
   {"AddModuleConfig",  AddModConfig, F_ADD_MOD,             FUNC_NO_WINDOW},
   {"Beep",         Bell,             F_BEEP,                FUNC_NO_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
 #ifdef BORDERSTYLE
   {"BorderStyle",  SetBorderStyle,   F_BORDERSTYLE,         FUNC_NO_WINDOW},
 #endif
   {"ButtonStyle",  ButtonStyle,      F_BUTTON_STYLE,        FUNC_NO_WINDOW},
 #ifdef USEDECOR
   {"ChangeDecor",  ChangeDecor,      F_CHANGE_DECOR,        FUNC_NEEDS_WINDOW},
-#endif
+#endif /* MS_DELETION_COMMENT */
+
+#if MS_DELETION_COMMENT
   {"ClickTime",    SetClick,         F_CLICK,               FUNC_NO_WINDOW},
   {"Close",        close_function,   F_CLOSE,               FUNC_NEEDS_WINDOW},
   {"ColormapFocus",SetColormapFocus, F_COLORMAP_FOCUS,      FUNC_NO_WINDOW},
@@ -63,9 +70,13 @@ struct functions func_config[] =
   {"DesktopSize",  SetDeskSize,       F_SETDESK,            FUNC_NO_WINDOW},
   {"Desk",         changeDesks_func, F_DESK,                FUNC_NO_WINDOW},
   {"Destroy",      destroy_function, F_DESTROY,             FUNC_NEEDS_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
 #ifdef USEDECOR
   {"DestroyDecor", DestroyDecor,     F_DESTROY_DECOR,	    FUNC_NO_WINDOW},
 #endif
+
+#if MS_DELETION_COMMENT
   {"DestroyFunc",  destroy_menu,     F_DESTROY_MENU,        FUNC_NO_WINDOW},
   {"DestroyMenu",  destroy_menu,     F_DESTROY_MENU,        FUNC_NO_WINDOW},
   {"DestroyModuleConfig", DestroyModConfig, F_DESTROY_MOD,  FUNC_NO_WINDOW},
@@ -74,10 +85,18 @@ struct functions func_config[] =
   {"EdgeResistance",SetEdgeResistance,F_EDGE_RES,           FUNC_NO_WINDOW},
   {"Exec",         exec_function,    F_EXEC,                FUNC_NO_WINDOW},
   {"ExecUseSHELL", exec_setup,       F_EXEC_SETUP,          FUNC_NO_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
   {"FlipFocus",    flip_focus_func,  F_FLIP_FOCUS,          FUNC_NEEDS_WINDOW},
+
+#if MS_DELETION_COMMENT
   {"Focus",        focus_func,       F_FOCUS,               FUNC_NEEDS_WINDOW},
   {"Function",     ComplexFunction,  F_FUNCTION,            FUNC_NO_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
   {"GlobalOpts",   SetGlobalOptions, F_GLOBAL_OPTS,         FUNC_NO_WINDOW},
+
+#if MS_DELETION_COMMENT
   {"GotoPage",     goto_page_func,   F_GOTO_PAGE,           FUNC_NO_WINDOW},
   {"HilightColor", SetHiColor,       F_HICOLOR,             FUNC_NO_WINDOW},
   {"Iconify",      iconify_function, F_ICONIFY,             FUNC_NEEDS_WINDOW},
@@ -87,12 +106,20 @@ struct functions func_config[] =
   {"Lower",        lower_function,   F_LOWER,               FUNC_NEEDS_WINDOW},
   {"Maximize",     Maximize,         F_MAXIMIZE,            FUNC_NEEDS_WINDOW},
   {"Menustyle",    SetMenuStyle,     F_MENUSTYLE,           FUNC_NO_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
   {"Menu",         staysup_func,     F_STAYSUP,             FUNC_POPUP},
+
+#if MS_DELETION_COMMENT
   {"ModulePath",   setModulePath,    F_MODULE_PATH,         FUNC_NO_WINDOW},
   {"Module",       executeModule,    F_MODULE,              FUNC_NO_WINDOW},
   {"Mouse",        ParseMouseEntry,  F_MOUSE,               FUNC_NO_WINDOW},
   {"Move",         move_window,      F_MOVE,                FUNC_NEEDS_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
   {"Nop",          Nop_func,         F_NOP,                 FUNC_NOP},
+
+#if MS_DELETION_COMMENT
   {"Next",         NextFunc,         F_NEXT,                FUNC_NO_WINDOW},
   {"None",         NoneFunc,         F_NONE,                FUNC_NO_WINDOW},
   {"OpaqueMoveSize", SetOpaque,      F_OPAQUE,              FUNC_NO_WINDOW},
@@ -100,10 +127,18 @@ struct functions func_config[] =
 #ifdef XPM
   {"PixmapPath",   setPixmapPath,    F_PIXMAP_PATH,         FUNC_NO_WINDOW},
 #endif
+#endif /* MS_DELETION_COMMENT */
+
   {"PopUp",        popup_func,       F_POPUP,               FUNC_POPUP},
+
+#if MS_DELETION_COMMENT
   {"Prev",         PrevFunc,         F_PREV,                FUNC_NO_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
   {"Quit",         quit_func,        F_QUIT,                FUNC_NO_WINDOW},
   {"QuitScreen",   quit_screen_func, F_QUIT_SCREEN,         FUNC_NO_WINDOW},
+
+#if MS_DELETION_COMMENT
   {"Raise_it",     raise_it_func,    F_RAISE_IT,            FUNC_NO_WINDOW},
   {"RaiseLower",   raiselower_func,  F_RAISELOWER,          FUNC_NEEDS_WINDOW},
   {"Raise",        raise_function,   F_RAISE,               FUNC_NEEDS_WINDOW},
@@ -119,11 +154,15 @@ struct functions func_config[] =
   {"Stick",        stick_function,   F_STICK,               FUNC_NEEDS_WINDOW},
   {"Send_WindowList",send_list_func, F_SEND_WINDOW_LIST,    FUNC_NO_WINDOW},
   {"Send_ConfigInfo",SendDataToModule, F_CONFIG_LIST,       FUNC_NO_WINDOW},
-  {"set_mask",     set_mask_function,F_SET_MASK,            FUNC_NO_WINDOW},
+  {"set_mask",     set_mask_function,F_SET_MASK,            FUNC_NO_WINDOW}, 
   {"Style",        ProcessNewStyle,  F_STYLE,               FUNC_NO_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
   {"Title",        Nop_func,         F_TITLE,               FUNC_TITLE},
   {"Titlestyle",   SetTitleStyle,    F_TITLESTYLE,          FUNC_NO_WINDOW},
   {"UpdateDecor",  UpdateDecor,      F_UPDATE_DECOR,        FUNC_NO_WINDOW},
+
+#if MS_DELETION_COMMENT
   {"Wait",         wait_func,        F_WAIT,                FUNC_NO_WINDOW},
   {"WarpToWindow", warp_func,        F_WARP,                FUNC_NEEDS_WINDOW},
   {"WindowFont",   LoadWindowFont,   F_WINDOWFONT,          FUNC_NO_WINDOW},
@@ -135,11 +174,13 @@ struct functions func_config[] =
   {"WindowsDesk",  changeWindowsDesk,F_CHANGE_WINDOWS_DESK, FUNC_NEEDS_WINDOW},
   {"XORValue",     SetXOR,           F_XOR,                 FUNC_NO_WINDOW},
   {"+",            add_another_item, F_ADDMENU2,            FUNC_NO_WINDOW},
+#endif /* MS_DELETION_COMMENT */
+
   {"",                    0,0}
 };
   
 
-
+#if MS_DELETION_COMMENT
 /***********************************************************************
  *
  *  Procedure:
@@ -215,7 +256,7 @@ void ExecuteFunction(char *Action, ScwmWindow *tmp_win, XEvent *eventp,
   return;
 }
 
-
+#endif /* MS_DELETION_COMMENT */
 
 
 int find_func_type(char *action)
