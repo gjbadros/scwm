c82fbd15a2eb5fafaccf625cf2086353b83fd3be
Maciej Stachowiak <mstachow@alum.mit.edu>
1997-10-26 05:45:08 +0000

    Modified configure.in and acconfig.h to detect where the installed guile is with respect to some guile interfaces that have evolved, specifically gh_vref vs. gh_vector_ref and scm_puts vs. scm_gen_puts. With some changes to the source, compiling against the lates guile should work.
    CVS:----------------------------------------------------------------------
    CVS:----------------------------------------------------------------------

--- acconfig.h
+++ acconfig.h
@@ -8,3 +8,13 @@
    970928.
  */
 #undef HAVE_SAFE_SCM_EVAL_STRING
+
+/* Define this if your libguile exports scm_puts, meaning that
+   scm_gen_puts should no longer be used. This should be true of
+   snapshots newer than 971014.  */
+#undef HAVE_SCM_PUTS
+
+/* Define this if your libguile has gh_vector_ref instead of gh_vref,
+   meaning that gh_vref should no longer be used. This should be
+   true of snapshots newer than 971012.  */
+#undef HAVE_GH_VECTOR_REF
