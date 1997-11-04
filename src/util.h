d68bdcb8908ba3ea7d0be198bc0b268045b544c0
Maciej Stachowiak <mstachow@alum.mit.edu>
1997-11-04 18:46:02 +0000

    Added prototype for redraw_borders.

--- src/util.h
+++ src/util.h
@@ -2,9 +2,8 @@
 #define UTIL_H
 
 void redraw_titlebars(ScwmDecor * fl, int extra_height);
-
+void redraw_borders(ScwmDecor *fl) ;
 void refresh_common(Window win_or_root);
-
 SCM call_thunk_with_message_handler(SCM thunk);
 
 #endif	/* UTIL_H */
