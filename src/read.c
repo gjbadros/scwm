d093cb13bffbf962812eef6a64b3e67c85a9036f
Greg J. Badros <gjbadros@sourceforge.net>
1997-11-05 08:15:56 +0000

    Removed; obsoleted

--- src/read.c
+++
@@ -1,8 +0,0 @@
-/****************************************************************************
- * This module is all original code 
- * by Rob Nation 
- * Copyright 1993, Robert Nation
- *     You may use this code for any purpose, as long as the original
- *     copyright remains in the source code and all documentation
- ****************************************************************************/
-
