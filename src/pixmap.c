165917e77dadbbbf7ce2780d9c5e1dea17227ee6
Greg J. Badros <gjbadros@sourceforge.net>
1997-11-08 22:21:42 +0000

    Added, only the outline for the object. Actually will probably rework this into a Picture object, and integrate the code in Picture.c, but I want to get the rest of these changes committed now as they're moderately pervasive.

---
+++ src/pixmap.c
@@ -0,0 +1,44 @@
+/* $Id$
+ * pixmap.c of SCWM
+ */
+
+#define PIXMAP_IMPLEMENTATION
+
+#include <config.h>
+#include <stdio.h>
+#include <guile/gh.h>
+#include <X11/keysym.h>
+#include "scwm.h"
+#include "errors.h"
+#include "pixmap.h"
+
+
+SCM 
+mark_pixmap(SCM obj)
+{
+  
+}
+
+size_t 
+free_pixmap(SCM obj)
+{
+  
+}
+
+int 
+print_pixmap(SCM obj, SCM port, scm_print_state * pstate)
+{
+  
+}
+
+SCM 
+pixmap_p(SCM obj)
+{
+  
+}
+
+SCM 
+make_pixmap(SCM pixmap_filename)
+{
+  
+}
