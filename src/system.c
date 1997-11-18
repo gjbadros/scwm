8b496838e0a978f11f36333fc80ba353855e8eb4
Maciej Stachowiak <mstachow@alum.mit.edu>
1997-11-18 02:07:40 +0000

    Changed sleep_ms to use usleep() where available, otherwise fall back to select().

--- src/system.c
+++ src/system.c
@@ -112,15 +112,12 @@ safemalloc(int length)
  *
  *************************************************************************/
 
-/* FIXGJB:  Andrei Tcherepanov <tandr@ptc.com> notes that the
-   below select line does not work for him on solaris;
-   we should probably only use select for platforms w/o
-   usleep (from <unistd.h>), and just use usleep(n*1000) when
-   we've got it */
-
 void 
 sleep_ms(int n)
 {
+#ifdef HAVE_USLEEP
+  usleep((unsigned long) n);
+#else /* !HAVE_USLEEP */
   struct timeval value;
 
   if (n <= 0)
@@ -130,6 +127,7 @@ sleep_ms(int n)
   value.tv_sec = n / 1000;
 
   (void) select(1, 0, 0, 0, &value);
+#endif /* HAVE_USLEEP */
 }
 
 
