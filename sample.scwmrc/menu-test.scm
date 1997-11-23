bab0e730675ce958ae12b7ebc7e7b547fa8ef71c
Greg J. Badros <gjbadros@sourceforge.net>
1997-11-23 09:36:05 +0000

    Added for testing new ScwmMenus

---
+++ sample.scwmrc/menu-test.scm
@@ -0,0 +1,20 @@
+;;; menu-item test cases
+(define sticky-menu-item
+  (make-menu-item "Sticky - this is a long one" toggle-stick "C-S-F8" #f
+		  (make-image "unknown.xpm") #f "sticky"))
+
+(define move-menu-item
+  (make-menu-item "Move" interactive-move "C-S-F7" #f
+		  (make-image "mini-move.xpm") #f "move"))
+
+(define resize-menu-item
+  (make-menu-item "Resize" interactive-resize "C-S-F8" #f
+		  (make-image "mini-resize.xpm") #f "resize"))
+
+(define a-menu
+  (make-scwm-menu (list sticky-menu-item move-menu-item resize-menu-item)
+		  (make-image "linux-menu.xpm") (load-color "blue")
+		  (load-color "gray80")))
+
+
+(popup-menu a-menu)
