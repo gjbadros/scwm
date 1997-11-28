5b9bd1b2f4747a67f70c9f12973af97a03cdd109
Greg J. Badros <gjbadros@sourceforge.net>
1997-11-28 08:52:37 +0000

    menu-test.scm: Added some test cases for bind-mouse and unbind-mouse, also for (image-properties ..)
    gjb.scwmrc: Added auto-raise using the broadcast hooks, uses
    module-types module

--- sample.scwmrc/menu-test.scm
+++ sample.scwmrc/menu-test.scm
@@ -2,21 +2,31 @@
 
 (define sticky-menu-item
   (make-menu-item "Sticky - this is a long one" toggle-stick "C-S-F8" #f
-		  (make-image "mini-stick.xpm") #f "sticky"))
+		  (make-picture "mini-stick.xpm") #f "sticky"))
 
 ;; Use a pixmap separator is just temporary -- it has lots of problems
 (define move-menu-item
   (make-menu-item "Move" interactive-move "C-S-F7" (make-image "separator.xbm")
-		  (make-image "mini-move.xpm") #f "move"))
+		  (make-picture "mini-move.xpm") #f "move"))
 
 (define resize-menu-item
   (make-menu-item "Resize" interactive-resize "C-S-F8" #f
-		  (make-image "mini-resize.xpm") #f "resize"))
+		  (make-picture "mini-resize.xpm") #f "resize"))
 
 (define a-menu
   (make-scwm-menu (list sticky-menu-item move-menu-item resize-menu-item)
-		  (make-image "linux-menu.xpm") (load-color "blue")
+		  (make-picture "linux-menu.xpm") (load-color "blue")
 		  (load-color "gray80")))
 
-
 (popup-menu a-menu)
+
+;; Test binding and unbind mouse button 1
+(bind-mouse 'root 1 (lambda () (display "foo\n")))
+
+(unbind-mouse 'root 1)
+
+;;(window-id)
+;;(raise-window (window-from-window-id 16777234))
+
+(define pic-unknown (make-image "unknown1.xpm"))
+(image-properties pic-unknown)
