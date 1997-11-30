ba216c40be4cc7d25493635119682b4f6b2d0588
Greg J. Badros <gjbadros@sourceforge.net>
1997-11-30 20:10:19 +0000

    system.scwmrc, gjb.scwmrc, menu-test.scm: Use new menuing code, use image-load-path, not set-image-path!
    Highlights of user-level changes to menu stuff:
    
    Use (menu ...) not (make-menu ...); first arg to menu is a real list [
    use (list ...) ]
    
    menu-title and menu-separator are variables now, not functions --
    e.g., use `menu-separator', not `(menu-separator)'; menuitem no longer
    is spelled `menu-item'
    
    keywords for menuitem are now #:image-left, #:image-above, not
    #:left-pixmap #:above-pixmap
    
    new keywords for menu include #:image-side (for new side image
    feature) and #:color-bg-image-side.

--- sample.scwmrc/menu-test.scm
+++ sample.scwmrc/menu-test.scm
@@ -30,7 +30,7 @@
 		  "moreoptions"))
 
 (define a-menu
-  (make-scwm-menu (list 
+  (make-menu (list 
 		   window-ops-title-item bitmap-separator
 		   sticky-menu-item move-menu-item resize-menu-item
 		   more-menu-ops-item)
@@ -38,7 +38,7 @@
 		  (load-color "gray80")))
 
 (define a-popup-menu 
-  (make-scwm-menu 
+  (make-menu 
    (list
     (make-menu-item "Iconify/Restore" toggle-iconify "C-S-Down" #f
 		    (make-image "mini-iconify.xpm") #f #f
@@ -49,8 +49,8 @@
    #f #f (load-color "gray80"))
    )
 
-(popup-menu a-menu)
-(popup-menu a-popup-menu)
+;;(popup-menu a-menu)
+;;(popup-menu a-popup-menu)
 
 ;;(menuitem-properties (caar (menu-properties a-popup-menu)))
 ;;(menu-properties a-menu)
