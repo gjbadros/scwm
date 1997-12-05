0de6b47c23ae8f81eb25c964c06c433a364910a8
Greg J. Badros <gjbadros@sourceforge.net>
1997-12-05 02:49:32 +0000

    Use new menu interface (make-menuitem) instead of (make-menu-item), and some more testing code.

--- sample.scwmrc/menu-test.scm
+++ sample.scwmrc/menu-test.scm
@@ -2,30 +2,58 @@
 
 ;; Use a pixmap separator is just temporary -- it has some problems
 (define bitmap-separator
-  (make-menu-item "" #f #f (make-image "separator.xbm")))
+  (make-menuitem "" #f #f (make-image "separator.xbm")))
 
 ;;(define bitmap-separator
 ;;  (make-menu-item "" #f #f (make-image "default.xbm")))
 (define window-ops-title-item
-  (make-menu-item "Window Operations" #f))
+  (make-menuitem "Window Operations" #f))
 
 (define sticky-menu-item
-  (make-menu-item "Sticky - this is a long one" toggle-stick "C-S-F8" #f
+  (make-menuitem "Sticky - this is a long one" toggle-stick "C-S-F8" #f
 		  (make-image "mini-stick.xpm") #f #f "sticky"))
 
 (define move-menu-item
-  (make-menu-item "Move" interactive-move "C-S-F7" #f
+  (make-menuitem "Move" interactive-move "C-S-F7" #f
 		  (make-image "mini-move.xpm") #f #f "move"))
 
 (define resize-menu-item
-  (make-menu-item "Resize" interactive-resize "C-S-F8" #f
+  (make-menuitem "Resize" interactive-resize "C-S-F8" #f
 		  (make-image "mini-resize.xpm") #f #f
 		  "resize"))
 
+(define a-popup-menu 
+  (make-menu 
+   (list
+    (make-menuitem "Iconify/Restore" toggle-iconify "C-S-Down" #f
+		    (make-image "mini-iconify.xpm") #f #f
+		    "iconify")
+    (make-menuitem "Stick/Unstick" (lambda () 
+					   (display "toggle-stick\n")) #f #f
+		    (make-image "mini-stick.xpm") #f #f
+		    "stick"))
+   #f #f (load-color "gray80"))
+   )
+
+(define b-popup-menu 
+  (make-menu 
+   (list
+    (make-menuitem "More more... " a-popup-menu "" #f #f
+		   #f #f "moremore")
+    (make-menuitem "Iconify/Restore" toggle-iconify "C-S-Down" #f
+		    (make-image "mini-iconify.xpm") #f #f
+		    "iconify")
+    (make-menuitem "Stick/Unstick" (lambda () 
+					   (display "toggle-stick\n")) #f #f
+		    (make-image "mini-stick.xpm") #f #f
+		    "stick"))
+   #f #f (load-color "gray80"))
+   )
+
 (define more-menu-ops-item
-  (make-menu-item "More options..." (lambda () (popup-menu a-popup-menu)) "" #f
+  (make-menuitem "More options..." b-popup-menu "" #f
 		  #f 
-		  (lambda () (display "hover\n"))
+		  (lambda () (display "uhover\n"))
 		  (lambda () (display "unhover\n"))
 		  "moreoptions"))
 
@@ -37,17 +65,6 @@
 		  (make-image "linux-menu.xpm") (load-color "blue")
 		  (load-color "gray80")))
 
-(define a-popup-menu 
-  (make-menu 
-   (list
-    (make-menu-item "Iconify/Restore" toggle-iconify "C-S-Down" #f
-		    (make-image "mini-iconify.xpm") #f #f
-		    "iconify")
-    (make-menu-item "Stick/Unstick" toggle-stick "" #f
-		    (make-image "mini-stick.xpm") #f #f
-		    "stick"))
-   #f #f (load-color "gray80"))
-   )
 
 ;;(popup-menu a-menu)
 ;;(popup-menu a-popup-menu)
@@ -56,9 +73,9 @@
 ;;(menu-properties a-menu)
 
 ;; Test binding and unbind mouse button 1
-(bind-mouse 'root 1 (lambda () (display "foo\n")))
+(bind-mouse 'root 1 (lambda () (popup-menu a-menu)))
 
-(unbind-mouse 'root 1)
+;;(unbind-mouse 'root 1)
 
 ;;(window-id)
 ;;(raise-window (window-from-window-id 16777234))
