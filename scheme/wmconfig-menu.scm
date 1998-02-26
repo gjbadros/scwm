9726281a50a9c44298534f9ae00c360b8450b87e
Maciej Stachowiak <mstachow@alum.mit.edu>
1998-02-26 03:33:14 +0000

    New module contributed by Sam Steingold <sds@usa.net> - this provides the function make-wmconfig-menu, which parses the Red Hat 5.0 /etc/X11/wmconfig directory and returns a mennu which reflects its contents. The title and name of the menu, and the directory to scan, may optionally be specified in that order. Appropriate defaults are provided.

---
+++ scheme/wmconfig-menu.scm
@@ -0,0 +1,92 @@
+;;;; 	Copyright (C) 1998 Sam Steingold and Maciej Stachowiak
+;;;; 
+;;;; This program is free software; you can redistribute it and/or modify
+;;;; it under the terms of the GNU General Public License as published by
+;;;; the Free Software Foundation; either version 2, or (at your option)
+;;;; any later version.
+;;;; 
+;;;; This program is distributed in the hope that it will be useful,
+;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;; GNU General Public License for more details.
+;;;; 
+;;;; You should have received a copy of the GNU General Public License
+;;;; along with this software; see the file COPYING.  If not, write to
+;;;; the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+;;;; Boston, MA 02111-1307 USA
+;;;; 
+
+
+
+(define-module (app scwm wmconfig-menu)
+  :use-module (app scwm base)
+  :use-module (app scwm optargs))
+
+
+
+;;; -----------------------------------------------------------------------
+;;; Process the /etc/X11/wmconfig/ directory in SCWM. For RedHat5 GNU/Linux.
+;;; -----------------------------------------------------------------------
+(define default-wmconfig-dir "/etc/X11/wmconfig/")
+(define default-wmconfig-title "System WM Config")
+(define default-wmconfig-name "WM Conf&ig")
+
+(define (wmc-fix str)		; kill the ampersand in the string
+  (do ((ll (length str))
+       (ix (string-index str #\& 0) (string-index str #\& ix)))
+      ((not ix) str) (string-set! str ix #\ )))
+
+(define (wmc-process-file fl)	; return (group . menuitem)
+  (let ((fd (open-input-file fl)))
+    (do ((wd (read fd) (read fd)) (mi ()) (gr ()) (nm ()))
+	((eof-object? wd) (close-input-port fd)
+	 (cons gr (if (eq? nm ()) () (apply menuitem nm mi))))
+      (cond ((string=? wd "mini-icon")
+	     (set! mi (append (list #:image-left (string (read fd))) mi)))
+	    ((string=? wd "exec")
+	     (set! mi (append (list #:action (exe (wmc-fix (read fd)))) mi)))
+	    ((string=? wd "name") (set! nm (read fd)))
+	    ((string=? wd "group") (set! gr (string (read fd))))))))
+
+(define (wmc-process-directory)
+  (display "Processing directory: ") (display wmconfig-dir)
+  (cond ((access? wmconfig-dir R_OK)
+	 (display ": directory readable...") (newline)
+	 (let ((wmcd (opendir wmconfig-dir)) (res ()))
+	   (do ((fl (readdir wmcd) (readdir wmcd)))
+	       ((eof-object? fl) (closedir wmcd) (newline) res)
+	     (display " ") (display fl)
+	     (set! fl (string-append wmconfig-dir fl))
+	     (cond ((access? fl R_OK)
+		    (set! fl (wmc-process-file fl))
+		    (if (eq? (car fl) ()) ()
+			(let ((ff (assoc (car fl) res)))
+			  (cond (ff (set-cdr! ff (cons (cdr fl) (cdr ff))))
+				(#t (set-cdr! fl (list (cdr fl)))
+				    (set! res (cons fl res))))))
+		    (display "+"))
+		   (#t (display "-"))))))
+	(#t (display ": directory unreadable!!!") (newline) ())))
+
+(define (wmc-regroup ls)	; regroup the menu items
+  (let ((main ()) (sub ()))
+    (for-each (lambda (me)
+		(if (string-index (car me) #\/) (set! sub (cons me sub))
+		    (set! main (cons me main)))) ls)
+    (for-each (lambda (me)
+		(let* ((pos (string-index (car me) #\/))
+		       (ww (substring (car me) 0 pos))
+		       (nn (substring (car me) (+ 1 pos) (length (car me))))
+		       (tt (assoc ww main)))
+		  (set! me (menuitem nn #:action (menu (cdr me))))
+		  (if tt (set-cdr! tt (cons me (cdr tt)))
+		      (set! main (cons (cons ww (list me)) main)))))
+	      sub)
+    (map (lambda (me) (menuitem (car me) #:action (menu (cdr me)))) main)))
+
+(define*-public (make-wmconfig-menu 
+		 #&optional (wmconfig-title default-wmconfig-title)
+		 (wmconfig-name default-wmconfig-name)
+		 (wmconfig-dir default-wmconfig-dir))
+  (menu (append! (list (menuitem wmconfig-title #f) menu-title menu-separator)
+		 (wmc-regroup (wmc-process-directory)))))
