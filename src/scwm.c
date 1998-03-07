82aee43998e08fcd2de61089fa81ebc70306af68
Maciej Stachowiak <mstachow@alum.mit.edu>
1998-03-07 04:41:58 +0000

    Provide scm_parse_path if not available.

--- src/scwm.c
+++ src/scwm.c
@@ -1590,8 +1590,34 @@ void init_scwm_load_path()
   *loc_load_path = path;
 }
 
+#ifndef HAVE_SCM_PARSE_PATH
+SCM
+scm_parse_path (char *path, SCM tail)
+{
+  if (path && path[0] != '\0')
+    {
+      char *scan, *elt_end;
+
+      /* Scan backwards from the end of the string, to help
+         construct the list in the right order.  */
+      scan = elt_end = path + strlen (path);
+      do {
+        /* Scan back to the beginning of the current element.  */
+        do scan--;
+        while (scan >= path && *scan != ':');
+        tail = scm_cons (scm_makfromstr (scan + 1, elt_end - (scan + 1), 0),
+                         tail);
+        elt_end = scan;
+      } while (scan >= path);
+    }
+
+  return tail;
+}
+#endif /* HAVE_SCM_PARSE_PATH */
+
 
 /* Local Variables: */
 /* tab-width: 8 */
 /* c-basic-offset: 2 */
 /* End: */
+
