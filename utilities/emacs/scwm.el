aa496c09e82789163fa583d6641b51ce3339678e
Sam Steingold <sds@goems.com>
1998-03-19 21:12:47 +0000

    Fixed scwm-run to restart in the same buffer after a crash. Completition, help and apropos are completely re-written. CVS: CVS:

--- utilities/emacs/scwm.el
+++ utilities/emacs/scwm.el
@@ -1,9 +1,15 @@
-;;; File: <scwm.el - 1998-03-03 Tue 08:33:34 EST sds@mute.eaglets.com>
+;;; File: <scwm.el - 1998-03-17 Tue 15:51:04 EST sds@mute.eaglets.com>
 ;;;
 ;;; Copyright (c) 1998 by Sam Shteingold <sds@usa.net>
 ;;;
 ;;; Completion-support added by Greg J. Badros <gjb@cs.washington.edu>
 ;;;    03/11/98 gjb
+;;;
+;;; Completition, help and apropos are completely re-worked by sds
+;;;	1998-03-13 Fri 13:58:16 EST	sds
+;;;
+;;; Fixed scwm-run to restart in the same buffer after a crash.
+;;;	1998-03-17 Tue 15:50:55 EST	sds
 ;;;
 ;;; This file is distributed under the GPL. See
 ;;;	<URL:http://www.gnu.ai.mit.edu/copyleft/gpl.html>
@@ -24,147 +30,153 @@
 ;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 ;; Boston, MA 02111-1307, USA.
 
-;;; Intructions to the user; put this file somewhere in your load-path
+;; Intructions to the user; put this file somewhere in your load-path
 ;; and add the following lines in your .emacs:
 
-;; (load "scwm")
-;; (define-key scheme-mode-map "\C-j" 'scwm-eval-print)
-;; (define-key scheme-mode-map "\C-x\C-j" 'scwm-eval-to-minibuffer)
-;; (define-key scheme-mode-map "\C-hd" 'scwm-procedure-documentation)
-;; (define-key scheme-mode-map [tab] 'scwm-complete-symbol)
+;; (require 'scwm)
 
-;; Now you do M-x scwm-run to get the *scwm* buffer, where you can type
-;; commands to be sent to scwm; while you can type C-x C-e in your
-;; scheme buffers to send the last SEXP to the interpreter. See C-h v
-;; scheme-buffer for details on running several scheme interpreters at
-;; once.
+;; Now you do M-x scwm-run (C-c C-s) to get the *scwm* buffer, where you
+;; can type commands to be sent to scwm; while you can type C-x C-e in
+;; your scheme buffers to send the last SEXP to the interpreter.
+;; See C-h v scheme-buffer for details on running several scheme
+;; interpreters at once.
 
 ;; *Alternatively* (that's OR, not XOR) you can type C-j evaluate the
 ;; last SEXP and insert the results at point, or type C-x C-j to display
 ;; them in the minibuffer.  This functionality does not require
 ;; preliminary M-x scwm-run.  Note that you can find your recent
-;; minibuffer messages in the buffer *Messages*.
+;; minibuffer messages in the buffer *Messages*.  Help and Apropos are
+;; also available: type C-h C-a for apropos and C-h C-s for documantation.
+;; Type M-TAB to complete symbol at point.
+
+;;; user variables
+;;; ---- ---------
 
 (defvar scwm-repl "/usr/local/bin/scwmrepl" "The path to scwmrepl.")
-
 (defvar scwm-exec "/usr/local/bin/scwmexec" "The path to scwmexec.")
 
+;;; pacify the compiler (XEmacs only)
+(eval-and-compile (autoload 'id-select-symbol "id-select"))
+
+;;; XEmacs doesn't have thingatpt. Too bad.
+(unless (fboundp 'thing-at-point)
+  (defun thing-at-point (what)
+    "Return the thing at point."
+    (unless (eq what 'symbol)
+      (error "crippled `thing-at-point' - symbols only"))
+    (let ((zz (id-select-symbol (point))))
+      (buffer-substring-no-properties (car zz) (cdr zz)))))
+
+;;; user functions
+;;; ---- ---------
+
+;;;###autoload
 (defun scwm-run ()
-  "Run scwm interaction.
+  "Run scwm interaction or pop to an existing one.
 Use \\[scheme-send-last-sexp] to eval the last sexp there."
   (interactive)
-  (run-scheme scwm-repl)
-  (set-buffer scheme-buffer)
-  (rename-buffer "*scwm*")
-  (setq scheme-buffer "*scwm*"))
+  (pop-to-buffer (setq scheme-buffer (make-comint "scwm" scwm-repl))))
 
+(defsubst scwm-eval (sexp out)
+  "Evaluate the SEXP with scwm-exec and print the results to OUT."
+  (call-process scwm-exec nil out nil sexp))
+
+(defsubst scwm-eval-last (out)
+  "Evaluate the last sexp with scwm-exec and print the results to OUT."
+  (scwm-eval (buffer-substring-no-properties
+	      (point) (save-excursion (backward-sexp) (point))) out))
+
+;;;###autoload
 (defun scwm-eval-print ()
   "Evaluate the last SEXP and insert the result into the current buffer."
   (interactive) (newline-and-indent)
-  (call-process scwm-exec nil t nil
-		(buffer-substring-no-properties
-		 (point) (save-excursion (backward-sexp) (point))))
-  (newline))
+  (scwm-eval-last t) (newline))
 
+;;;###autoload
 (defun scwm-eval-to-minibuffer ()
   "Evaluate the last SEXP and show the result in the minibuffer."
   (interactive)
-  (let* ((start (point))
-	 (end (save-excursion (backward-sexp) (point)))
-	 (arg (buffer-substring-no-properties start end))
-	 (string (with-output-to-string
-		   (call-process scwm-exec nil standard-output nil arg))))
-    (message string)))
+  (message (with-output-to-string (scwm-eval-last standard-output))))
 
-(defun make-list-of-symbols ()
-  "Return a list of all the s-expressions in the current buffer from point on"
-  (let ((sexp-list nil)
-	(old-point (point))
-	(done nil))
-    (while (not done)
-      (forward-sexp)
-      (if (looking-at ")")
-	  (setq done 't))
-      (setq sexp-list (cons (intern (buffer-substring-no-properties old-point (point))) sexp-list))
-      (setq old-point (+ 1 (point))))
-    sexp-list))
+(defalias 'advertised-xscheme-send-previous-expression
+    'scwm-eval-to-minibuffer)
 
-;; This is rough, but can be the foundation of something
-;; a lot fancier and cleaner
-(defun scwm-complete-symbol ()
-  "Complete the current symbol by querying scwm using apropos-internal."
+;;; completion
+;;; ----------
+
+(defvar scwm-obarray nil "The obarray for scwm completion.")
+(defvar scwm-history nil "The input history of SCWM completions.")
+
+(defun scwm-make-obarray ()
+  "Create and return an obarray of SCWM symbols."
+  ;; can't use read-from-string because "? " is read as 32
+  (let ((obarray (make-vector 67 0)) (pos 2)
+	(tb (get-buffer-create " *scwm-obarray*")))
+    (set-buffer tb) (erase-buffer)
+    (scwm-eval "(apropos-internal \"*\")" tb)
+    (goto-char 1)
+    (while (search-forward " " nil t)
+      (intern (buffer-substring-no-properties pos (1- (point))) obarray)
+      (setq pos (point)))
+    obarray))
+
+(defun scwm-complete-symbol (&optional sym)
+  "Complete the current symbol by querying scwm using apropos-internal.
+Returns a string."
+  (setq sym (or sym (thing-at-point 'symbol))
+	scwm-obarray (or scwm-obarray (scwm-make-obarray)))
+  (completing-read "SCWM symbol: " scwm-obarray nil nil sym scwm-history sym))
+
+;;;###autoload
+(defun scwm-complete-symbol-insert ()
   (interactive)
-  (let* ((start (point))
-	 (end (save-excursion (forward-sexp -1) (point)))
-	 (arg (buffer-substring-no-properties start end))
-	 (bfr (get-buffer-create "*scwm-completions*"))
-	 (choices (save-excursion
-		     (set-buffer bfr)
-		     (erase-buffer)
-		     (lisp-mode)
-		     (call-process scwm-exec nil t nil (concat "(apropos-internal \"^" arg "\")"))
-		     (goto-char 2)
-		     (make-list-of-symbols))))
-    ;; This cannot possibly be right, but it works for now
-    (completer-complete-goto
-     "^ \t\n\(\)[]{}'`" completer-words (vconcat choices) (lambda (test) t))))
+  (let* ((end (point)) (beg (save-excursion (backward-sexp) (point)))
+	 (pat (buffer-substring-no-properties beg end))
+	 (comp (try-completion pat scwm-obarray)))
+    (cond ((eq comp t) (message "`%s' is complete" pat))
+	  ((null comp) (message "Cannot complete `%s'" pat) (ding))
+	  ((not (string= comp pat)) (delete-region beg end) (insert comp))
+	  (t (message "Making completion list...")
+	     (with-output-to-temp-buffer "*Completions*"
+	       (display-completion-list (all-completions pat scwm-obarray)))
+	     (message "Making completion list...done")))))
 
-;;; Make C-x C-e do the right thing
-(defun advertised-xscheme-send-previous-expression ()
-  (interactive)
-  (scwm-eval-to-minibuffer))
+;;; help
+;;; ----
 
-(defun scwm-procedure-documentation-1 (function out)
-  (call-process scwm-exec nil out nil (concat "(procedure-documentation " (symbol-name function) ")")))
-  
-;; from help.el
-(defun scwm-function-at-point ()
-  "Return the scwm-function whose name is around point.
-If that gives no function, return the function which is called by the
-list containing point.  If that doesn't give a function, return nil."
-  (or (condition-case ()
-	  (let ((stab (syntax-table)))
-	    (unwind-protect
-		(save-excursion
-		  (set-syntax-table emacs-lisp-mode-syntax-table)
-		  (or (not (zerop (skip-syntax-backward "_w")))
-		      (eq (char-syntax (char-after (point))) ?w)
-		      (eq (char-syntax (char-after (point))) ?_)
-		      (forward-sexp -1))
-		  (skip-chars-forward "`'")
-;;;		  (let ((obj (read (current-buffer))))
-;;;		    (and (symbolp obj) (fboundp obj) obj)))
-		  (read (current-buffer)))
-	      (set-syntax-table stab)))
-	(error nil))
-      (condition-case ()
-	  (save-excursion
-	    (save-restriction
-	      (narrow-to-region (max (point-min) (- (point) 1000))
-				(point-max))
-	      (backward-up-list 1)
-	      (forward-char 1)
-	      (let (obj)
-		(setq obj (read (current-buffer)))
-		(and (symbolp obj) (fboundp obj) obj))))
-	(error nil))))
+;;;###autoload
+(defun scwm-documentation (pat)
+  "Query scwm for documentation for the give symbol."
+  (interactive (list (scwm-complete-symbol)))
+  (with-output-to-temp-buffer "*Help*"
+    (princ "SCWM documentation `") (princ pat) (princ "':\n\n")
+    (scwm-eval (concat "(procedure-documentation " pat ")")
+	       standard-output)))
 
-;;; bind this to C-h d
-(defun scwm-procedure-documentation (function-name)
-  "Query scwm for documentation for function named function-name"
+;;;###autoload
+(defun scwm-apropos (pat)
+  "Apropos an scwm symbol."
+  ;; (interactive (interactive-token "SCWM Apropos"))
   (interactive
-    (let* ((fn (scwm-function-at-point))
-           (val (let ((enable-recursive-minibuffers t))
-                  (completing-read
-                    (if fn
-                        (format (gettext "Describe function (default %s): ")
-				fn)
-                        (gettext "Describe function: "))
-                    obarray 'fboundp t nil 'function-history))))
-      (list (if (equal val "") fn (intern val)))))
-  (with-displaying-help-buffer
-   (lambda ()
-     (scwm-procedure-documentation-1 function-name standard-output)
-     ;; Return the text we displayed.
-     (buffer-string nil nil standard-output))))
+   (list (read-string
+	  "SCWM Apropos: "
+	  (format "%s" (or (thing-at-point 'symbol)
+			   (progn (skip-syntax-backward (or skip "^w"))
+				  (if (bobp) () (backward-char 1))
+				  (thing-at-point 'symbol)) "")))))
+  (with-output-to-temp-buffer "*Apropos*"
+    (princ "SCWM apropos `") (princ pat) (princ "':\n\n")
+    (scwm-eval (concat "(apropos \"" pat "\")") standard-output)))
 
+;;; keybindings
+;;; -----------
+
+(define-key scheme-mode-map "\C-j" 'scwm-eval-print)
+(define-key scheme-mode-map "\C-c\C-s" 'scwm-run)
+(define-key scheme-mode-map "\C-x\C-j" 'scwm-eval-to-minibuffer)
+(define-key scheme-mode-map "\C-h\C-s" 'scwm-documentation)
+(define-key scheme-mode-map "\C-h\C-a" 'scwm-apropos)
+(define-key scheme-mode-map [(meta tab)] 'scwm-complete-symbol-insert)
+
+(provide 'scwm)
+;; scwm.el ends here
