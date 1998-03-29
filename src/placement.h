dafca9a5b496e6662cd06a732f1f401e05479b94
Greg J. Badros <gjbadros@sourceforge.net>
1998-03-29 22:38:44 +0000

    * placement.h: Added, for protos for placement.c

---
+++ src/placement.h
@@ -0,0 +1,19 @@
+/* $Id$
+ * placement.h
+ * SCWM
+ * (C) 1998 Maciej Stachowiak and Greg J. Badros
+ */
+
+#ifndef PLACEMENT_H
+#define PLACEMENT_H
+
+#include "window.h"
+
+Bool PlaceWindow(ScwmWindow *tmp_win, int Desk);
+
+#endif
+
+/* Local Variables: */
+/* tab-width: 8 */
+/* c-basic-offset: 2 */
+/* End: */
