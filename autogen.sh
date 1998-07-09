d2513ba42e89b5703a510064b5494d9493dfaeea
Robert Bihlmeyer <robbe@orcus.priv.at>
1998-07-09 13:18:11 +0000

    Added an autogen.sh. Modified the instructions in HACKING accordingly.

---
+++ autogen.sh
@@ -0,0 +1,19 @@
+## autogen.sh - generate all the twisty little files.
+
+# Generate aclocal.m4 from configure.in
+aclocal
+
+# Generate Makefile.in's from Makefile.am's and configure.in
+# (adding missing files that may be needed for proper operation)
+automake --add-missing
+
+# Generate include/config.h.in from acconfig.h and configure.in
+autoheader
+
+# Generate configure from configure.in and aclocal.m4
+autoconf
+
+# Detect system parameters and generate Makefiles from Makefile.in's
+# and include/config.h from include/config.h.in, enabling
+# maintainer-specific rules.
+./configure --enable-maintainer-mode "$@"
