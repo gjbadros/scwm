db52b435e106577cd0e53e33d1e380d137e79a6d
Greg J. Badros <gjbadros@sourceforge.net>
1998-07-10 17:06:54 +0000

    Added SCWM_EXTRACT_COMMENTS conditional to turn off expansion of the macros, and give example of how to use that to extract comments for primitives documentation using gcc -E -C

--- src/scwm-snarf.h
+++ src/scwm-snarf.h
@@ -6,6 +6,11 @@
 #ifndef SCWM_SNARF_H__
 #define SCWM_SNARF_H__
 
+/* Can extract comment post-cpp using, e.g.:
+gcc -DHAVE_CONFIG_H -I. -I. -I../include -I/usr/X11R6/include -I/uns/include -E -C -DSCWM_EXTRACT_COMMENTS binding.c
+*/
+
+#ifndef SCWM_EXTRACT_COMMENTS
 #ifndef SCM_MAGIC_SNARFER
 #define SCWM_PROC(fname,primname, req, opt, var, ARGLIST) \
 	SCM_PROC(s_ ## fname, primname, req, opt, var, fname); \
@@ -13,6 +18,7 @@ SCM fname ARGLIST
 #else
 #define SCWM_PROC(fname,primname, req, opt, var, ARGLIST) \
 	SCM_PROC(s_ ## fname, primname, req, opt, var, fname);
+#endif
 #endif
 
 #endif
