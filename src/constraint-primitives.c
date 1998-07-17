e7691615388181293506a647337a9fa54e178f0c
Greg J. Badros <gjbadros@sourceforge.net>
1998-07-17 03:06:13 +0000

    Add stays, debug code

--- src/constraint-primitives.c
+++ src/constraint-primitives.c
@@ -16,6 +16,7 @@
 #include "window.h"
 #include "ClLinearEquation.h"
 #include "ClSimplexSolver.h"
+#include <strstream>
 
 SCWM_PROC(keep_tops_even, "keep-tops-even", 2, 0, 0,
           (SCM winA, SCM winB))
@@ -34,15 +35,29 @@ WINA and WINB are both window objects */
   ScwmWindow const *const pswB = PSWFROMWIN(winB);
   const ClVariable &clvY_A = pswA->frame_y;
   const ClVariable &clvY_B = pswB->frame_y;
+  solver.addVar(clvY_A).addVar(clvY_B);
 
   ClLinearEquation eqY(clvY_A,clvY_B+0.0);
+  strstream ss;
+  ss << "Adding constraint: " << eqY << endl;
+  ss.freeze();
+  fprintf(stderr,"%s",ss.str());
+
   solver.addConstraint(eqY);
   /* FIXGJB: need to add hook to remove the constraint if winA or winB
      disappears */
   return SCM_UNSPECIFIED;
 }
 
-
+#if 0
+/* not needed -- can do in scheme */
+SCWM_PROC(resolve_and_reposition, "resolve-and-reposition", 0, 0, 0,
+          ())
+{
+  
+  return SCM_UNSPECIFIED;
+}
+#endif
 
 void
 init_constraint_primitives()
