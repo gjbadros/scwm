c7e674d6b0683e326d730ae1f31b77d530a6d713
Greg J. Badros <gjbadros@sourceforge.net>
1998-07-19 01:07:40 +0000

    More testing of new wrapped types

--- scheme/tests/constraints.scm
+++ scheme/tests/constraints.scm
@@ -1,11 +1,37 @@
 ;; keep two windows at same y coord
 (begin
+ (define w1 (get-window))
  (define w2 (get-window))
+ (add-stays-on-window w1)
  (add-stays-on-window w2)
  (add-stays-on-window (current-window-with-focus)))
 
+(add-stays-on-window (get-window))
+
 (keep-tops-even (current-window-with-focus) w2)
 (keep-to-left-of (current-window-with-focus) w2)
+(keep-to-left-of (current-window-with-focus) (get-window))
+(keep-to-left-of (current-window-with-focus) (get-window))
+
+(define v (make-cl-variable))
+(define e (make-cl-expression v))
+(define eq (make-cl-equation e))
+(define ineq (make-cl-inequality e))
+(define solver (make-cl-solver))
+
+(cl-variable? c)
+(cl-variable? w1)
+(cl-expression? e)
+(cl-expression? w1)
+(cl-equation? eq)
+(cl-equation? w1)
+(cl-inequality? ineq)
+(cl-inequality? w1)
+(cl-solver? solver)
+(cl-solver? w1)
+
+
+;; obsoleted stuff below here
 
 ;; can use window-list menu to see positions easily, but this works too
 (window-position (current-window-with-focus))
@@ -25,6 +51,8 @@
 	 (write w))
        (list-windows)))
 
+
+
 (define w1 (car (list-windows)))
 (define w2 (cadr (list-windows)))
 
@@ -36,6 +64,8 @@
   (reposition-all-windows))
 
 (bind-mouse 'all "C-S-M-2" my-interactive-move)
+
+
 
 
 
