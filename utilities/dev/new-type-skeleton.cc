9fa01f046c6a1f023608049e18510a7e2306e91b
Greg J. Badros <gjbadros@sourceforge.net>
1998-07-19 03:35:16 +0000

    Use auto-insert-tkld.el to manage the template more pleasantly

--- utilities/dev/new-type-skeleton.cc
+++ utilities/dev/new-type-skeleton.cc
@@ -1,10 +1,12 @@
 //// $Id$
 //// Skeleton to use for creating a new type for GUILE
 //// (C) 1998, By Greg J. Badros -- 18 July 1998
-// X = SCMTYPE (e.g., cl_variable)
-// P = SCMTYPE for primitive names (e.g., cl-variable)
-// Y = Type name (e.g., ClVariable)
-// H = Hungarian tag name (e.g, clv)
+// SCMTYPE: %[SCMTYPE (e.g., cl_variable -- a C identifier)? %]
+// SCM object name: %[SCM object name (e.g., cl-variable -- a Scheme id)? %]
+// Wrapped type name: %[Wrapped Type Name (e.g., ClVariable)? %]
+// Hungarian Tag: %[Hungarian tag name (e.g, clv)? %]
+// By %U
+// %d
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
@@ -12,13 +14,11 @@
 
 #include <guile/gh.h>
 
-#include <iostream.h>
 #include <assert.h>
-#include <strstream.h>
-#include "scwm-snarf.h"
+#include "scwm.h"
 
-#define CASSOWARY_SCM_IMPLEMENTATION
-#include "cassowary_scm.h"
+#define %F_IMPLEMENTATION
+#include "%b.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -32,52 +32,52 @@ inline SCM SCM_BOOL_FromF(bool f) { return (f? SCM_BOOL_T: SCM_BOOL_F); }
 inline bool FUnsetSCM(SCM scm) { return (scm == SCM_UNDEFINED || scm == SCM_BOOL_F); }
 
 #undef SCMTYPEID
-#define SCMTYPEID scm_tc16_XXXXXXXX
+#define SCMTYPEID scm_tc16_%1
 
 long SCMTYPEID;
 
-inline bool FIsTTTTTTTTSCM(SCM scm) 
+inline bool FIs%3SCM(SCM scm) 
 { return SCM_NIMP(scm) && SCM_CAR(scm) == (SCM) SCMTYPEID; }
 
-inline TTTTTTTT *PHHHHHHHHFromScm(SCM scm)
-{ return (TTTTTTTT *)(SCM_CDR(scm)); }
+inline %3 *P%4FromScm(SCM scm)
+{ return (%3 *)(SCM_CDR(scm)); }
 
 SCM
-mark_XXXXXXXX(SCM scm)
+mark_%1(SCM scm)
 {
   SCM_SETGC8MARK(scm);
   return SCM_BOOL_F;
 }
 
 size_t
-free_XXXXXXXX(SCM scm)
+free_%1(SCM scm)
 {
-  TTTTTTTT *pHHHHHHHH = PHHHHHHHHFromScm(scm);
-  delete pHHHHHHHH;
+  %3 *p%4 = P%4FromScm(scm);
+  delete p%4;
   return 0;
 }
 
 int
-print_XXXXXXXX(SCM scm, SCM port, scm_print_state *pstate)
+print_%1(SCM scm, SCM port, scm_print_state *pstate)
 {
   strstream ss;
-  TTTTTTTT *pHHHHHHHH = PHHHHHHHHFromScm(scm);
-  ss << "#<PPPPPPPP" << *pHHHHHHHH << ">" << ends;
+  %3 *p%4 = P%4FromScm(scm);
+  ss << "#<%2" << *p%4 << ">" << ends;
   scm_puts(ss.str(), port);
   return 1;
 }
 
-SCWM_PROC (XXXXXXXX_p, "PPPPPPPP?", 1, 0, 0,
+SCWM_PROC (%1_p, "%2?", 1, 0, 0,
            (SCM scm))
-#define FUNC_NAME s_XXXXXXXX_p
+#define FUNC_NAME s_%1_p
 {
-  return SCM_BOOL_FromF(FIsTTTTTTTTSCM(scm));
+  return SCM_BOOL_FromF(FIs%3SCM(scm));
 }
 #undef FUNC_NAME
 
-SCWM_PROC (make_XXXXXXXX, "make-PPPPPPPP", 0, 0, 0,
+SCWM_PROC (make_%1, "make-%2", 0, 0, 0,
            ())
-#define FUNC_NAME s_make_XXXXXXXX
+#define FUNC_NAME s_make_%1
 {
   int iarg = 1;
 
@@ -92,19 +92,41 @@ SCWM_PROC (make_XXXXXXXX, "make-PPPPPPPP", 0, 0, 0,
   }
 #endif
 
-  TTTTTTTT *pHHHHHHHH = new TTTTTTTT();
+  %3 *p%4 = new %3();
 
   SCM answer;
 
   SCM_DEFER_INTS;
   SCM_NEWCELL(answer);
   SCM_SETCAR(answer, (SCM) SCMTYPEID);
-  SCM_SETCDR(answer, (SCM) pHHHHHHHH);
+  SCM_SETCDR(answer, (SCM) p%4);
   SCM_ALLOW_INTS;
 
   return answer;
 }
 #undef FUNC_NAME
+
+
+SCWM_PROC (, , 0, 0, 0,
+           ())
+#define FUNC_NAME s_
+{
+  return SCM_UNDEFINED;
+}
+#undef FUNC_NAME
+
+
+MAKE_SMOBFUNS(%1);
+
+void
+init_%b()
+{
+  REGISTER_SCWMSMOBFUNS(%1);
+
+#ifndef SCM_MAGIC_SNARFER
+#include "%b.x"
+#endif
+}
 
 #ifdef __cplusplus
 } // extern "C"
