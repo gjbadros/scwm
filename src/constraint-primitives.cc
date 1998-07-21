b59037c08f8b344da43e6fd66ae1dd9bc20bcc6c
Greg J. Badros <gjbadros@sourceforge.net>
1998-07-21 01:02:24 +0000

    Renamed constraint-primitives.c to constraint-primitives.cc, since it is C++ code

--- src/constraint-primitives.cc
+++ src/constraint-primitives.cc
@@ -15,11 +15,15 @@
 #include "scwm.h"
 #include "constraint-primitives.h"
 #include "scwm-constraints.h"
+#include "scwm-constraints.hpp"
 #include "window.h"
 #include "ClLinearEquation.h"
 #include "ClLinearInequality.h"
 #include "ClSimplexSolver.h"
+#include "../guile/cassowary_scm.hpp"
 #include <strstream>
+
+ClSimplexSolver *psolver;
 
 SCWM_PROC(add_stays_on_window, "add-stays-on-window", 1, 0, 0,
           (SCM win))
@@ -33,21 +37,32 @@ WIN is a window objects */
   }
   
   ScwmWindow const *const psw = PSWFROMSCMWIN(win);
-  const ClVariable &clvX = psw->frame_x;
-  const ClVariable &clvY = psw->frame_y;
-  const ClVariable &clvW = psw->frame_width;
-  const ClVariable &clvH = psw->frame_height;
-  solver
-    .addPointStay(clvW,clvH,100)
-    .addPointStay(clvX,clvY,1);
+  ScwmWindowConstraintInfo *pswci = psw->pswci;
+  pswci->AddStays();
 
   /* FIXGJB: need to add hook to remove the stays if win disappears */
   return SCM_UNSPECIFIED;
 }
 #undef FUNC_NAME
 
+SCWM_PROC (scwm_set_master_solver, "scwm-set-master-solver", 1, 0, 0,
+           (SCM solver))
+     /** Use SOLVER as the master solver for scwm */
+#define FUNC_NAME s_scwm_set_master_solver
+{
+  int iarg = 1;
+  if (!FIsClSimplexSolverScm(solver))
+    scm_wrong_type_arg(FUNC_NAME,iarg++,solver);
+
+  psolver = PsolverFromScm(solver);
+  return SCM_UNDEFINED;
+}
+#undef FUNC_NAME
 
 
+
+
+// FIXGJB: below are obsoleted by general mechanism
 SCWM_PROC(keep_tops_even, "keep-tops-even", 2, 0, 0,
           (SCM winA, SCM winB))
   /** Keep the tops of the frames of winA and winB at the same y position.
@@ -64,16 +79,16 @@ WINA and WINB are both window objects */
   
   ScwmWindow const *const pswA = PSWFROMSCMWIN(winA);
   ScwmWindow const *const pswB = PSWFROMSCMWIN(winB);
-  const ClVariable &clvY_A = pswA->frame_y;
-  const ClVariable &clvY_B = pswB->frame_y;
-  solver.addVar(clvY_A).addVar(clvY_B);
+  const ClVariable &clvY_A = pswA->pswci->_frame_y;
+  const ClVariable &clvY_B = pswB->pswci->_frame_y;
+  psolver->addVar(clvY_A).addVar(clvY_B);
 
   ClLinearEquation eqY(clvY_A,clvY_B+0.0);
   strstream ss;
   ss << "Adding constraint: " << eqY << endl << ends;
   fprintf(stderr,"%s",ss.str());
 
-  solver.addConstraint(eqY);
+  psolver->addConstraint(eqY);
   /* FIXGJB: need to add hook to remove the constraint if winA or winB
      disappears */
   return SCM_UNSPECIFIED;
@@ -97,19 +112,19 @@ WINA and WINB are both window objects */
   
   ScwmWindow const *const pswA = PSWFROMSCMWIN(winA);
   ScwmWindow const *const pswB = PSWFROMSCMWIN(winB);
-  const ClVariable &clvX_A = pswA->frame_x;
-  const ClVariable &clvW_A = pswA->frame_width;
-  const ClVariable &clvX_B = pswB->frame_x;
-  const ClVariable &clvW_B = pswA->frame_width;
-  solver.addVar(clvX_A).addVar(clvX_B);
-  solver.addVar(clvW_A).addVar(clvW_B);
+  const ClVariable &clvX_A = pswA->pswci->_frame_x;
+  const ClVariable &clvW_A = pswA->pswci->_frame_width;
+  const ClVariable &clvX_B = pswB->pswci->_frame_x;
+  const ClVariable &clvW_B = pswA->pswci->_frame_width;
+  psolver->addVar(clvX_A).addVar(clvX_B);
+  psolver->addVar(clvW_A).addVar(clvW_B);
 
   ClLinearInequality ineqX(clvX_A+clvW_A,cnLEQ,clvX_B);
   strstream ss;
   ss << "Adding constraint: " << ineqX << endl << ends;
   fprintf(stderr,"%s",ss.str());
 
-  solver.addConstraint(ineqX);
+  psolver->addConstraint(ineqX);
   /* FIXGJB: need to add hook to remove the constraint if winA or winB
      disappears */
   return SCM_UNSPECIFIED;
@@ -117,12 +132,15 @@ WINA and WINB are both window objects */
 #undef FUNC_NAME
 
 
+extern "C" {
 void
 init_constraint_primitives()
 {
 #ifndef SCM_MAGIC_SNARFER
 #include "constraint-primitives.x"
 #endif
+}
+
 }
 
 #endif /* USE_CASSOWARY */
