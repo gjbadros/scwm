78a9105163dd2882e51858fe373edb32e3567e14
Greg J. Badros <gjbadros@sourceforge.net>
1998-07-21 22:57:07 +0000

    Removed obsoleted stuff.

--- scheme/tests/constraints.scm
+++ scheme/tests/constraints.scm
@@ -31,7 +31,6 @@
     (cl-add-constraint 
      solver (make-cl-constraint (cl-plus w1-width w1-x) <= w2-x))))
 
-
 (keep-bottoms-even (current-window-with-focus) w1)
 (keep-tops-even (current-window-with-focus) w2)
 (keep-to-left-of (current-window-with-focus) w1)
@@ -98,48 +97,6 @@ cls-required
 (cl-solver? solver)
 (cl-solver? w1)
 
-
-;; obsoleted stuff below here
-
-;; can use window-list menu to see positions easily, but this works too
-(window-position (current-window-with-focus))
-(window-position w)
-
-(define (reposition-window w)
-  (let ((wp (window-position w)))
-    (move-to (car wp) (cadr wp) w)))
-
-(define (reposition-all-windows)
-  (map (lambda (w)
-	 (reposition-window w))
-       (list-windows)))
-
-(define (write-all-windows)
-  (map (lambda (w)
-	 (write w))
-       (list-windows)))
-
-
-
-(define w1 (car (list-windows)))
-(define w2 (cadr (list-windows)))
-
-(reposition-window w1)
-(reposition-window w2)
-
-(define (my-interactive-move w)
-  (interactive-move w)
-  (reposition-all-windows))
-
-(bind-mouse 'all "C-S-M-2" my-interactive-move)
-
-
-
-(stick (get-window))
-
-(map (lambda (w)
-       (move-to 0 0 w))
-     (list-windows))
 
 ;;; Local Variables:
 ;;; eval: (load "scwm")
