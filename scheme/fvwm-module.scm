d2991c2b57dccb3e08548f0db0276e82c1e95546
Maciej Stachowiak <mstachow@alum.mit.edu>
1998-07-23 23:30:46 +0000

    1998-07-23  Maciej Stachowiak  <maciej@roc-ny1-20.ix.netcom.com>
    	* fvwm-module.scm (add-window): Determine the icon file
    	correctly. Thanks to Danius Michalides for the bug report.
    
    1998-07-21  Maciej Stachowiak  <maciej@roc-ny4-02.ix.netcom.com>
    
    	* fvwm-module.scm: New interface to launching fvwm2 modules. This
    	should be a better base for fvwm2 emulation, as well as a more
    	convenient interface for users. Added/renamed/modified:
    
    	variable: fvwm2-module-path
    
    	register-fvwm2-module-config MODULE-TYPE . ARGS
    
    	append-fvwm2-module-config MODULE-TYPE . ARGS
    	clear-fvwm2-module-config MODULE-TYPE
    	get-fvwm2-module-config MODULE-TYPE
    	run-fvwm2-module MODULE-NAME #&optional OTHER-ARGS CONFIG-INFO
    	  CONFIG-FILE
    	kill-fvwm2-module FMOD
    	kill-all-fvwm2-modules
    	kill-fvwm2-modules-by-name MODULE-NAME
    
    	See the comment at the top of the file for more details.

--- scheme/fvwm-module.scm
+++ scheme/fvwm-module.scm
@@ -25,39 +25,98 @@
   :use-module (app scwm bincomm)
   :use-module (app scwm fvwm-eval)
   :use-module (app scwm optargs)
-  :use-module (app scwm decor))
+  :use-module (app scwm decor)
+  :use-module (ice-9 common-list))
 
 
 
 ;;   fvwm-module
 ;; Supports running fvwm2 modules from scwm - this is mostly useful for
-;; the pager. Here is an example of running a module in a scwmrc:
+;; the pager.
 ;;
-;;(define m1 (run-fvwm-module  
-;;	    ;; path to the module
-;;	    "/mit/windowmanagers/lib/X11/fvwm2/FvwmPager"
-;;	    ;; your .fvwm2rc (does not need to exist for most modules)
-;;	    "~/.fvwm2rc"
-;;	    ;; list of configuration lines
-;;	    '("*FvwmPagerBack grey76"
-;;	      "*FvwmPagerFore black"
-;;	      "*FvwmPagerHilight navyblue"
-;;	      "*FvwmPagerFont none"
-;;	      "*FvwmPagerDeskTopScale 40"
-;;	      "*FvwmPagerLabel 0 Top"
-;;	      "*FvwmPagerLabel 1 Bottom"
-;;	      "*FvwmPagerSmallFont 5x8")
-;;	    '("0" "2")))
+;; Public interface:
 ;;
-;; To later shut down the module, use kill-fvwm-module:
+;;   variable: fvwm2-module-path 
+;; This list of strings contains is used as the list of directories to
+;; search when running fvwm2 modules.
 ;;
-;;   (kill-fvwm-module m1)
+;;   register-fvwm2-module-config MODULE-TYPE . ARGS
+;; Set the configuration for fvwm modules named MODULE-TYPE to be the
+;; list of strings ARGS by default.
+;;
+;;   append-fvwm2-module-config MODULE-TYPE . ARGS
+;; Append the strings ARGS to the current default configuration for
+;; modules of type MODULE-TYPE
+;;
+;;   clear-fvwm2-module-config MODULE-TYPE
+;; Clear the default configuration for modules of type MODULE-TYPE 
+;;
+;;   get-fvwm2-module-config MODULE-TYPE
+;; Retrieve the current default configuration for modules of type 
+;; MODULE-TYPE 
+;;
+;;   run-fvwm2-module MODULE-NAME #&optional OTHER-ARGS CONFIG-INFO
+;;   CONFIG-FILE
+;; Run the module MODULE-NAME, searching the fvwm2-module-path to find
+;; it, unless MODULE-NAME starts with "/", "./" or "../", in which
+;; case it is taken as an absolute filename. OTHER-ARGS specifies the
+;; extra arguments to pass to the module. For example, the FvwmPager
+;; module requires two arguments specifying the first and last desk
+;; the pager should manage. All module arguments should be given as
+;; strings. The CONFIG-FILE and CONFIG-INFO arguments should not be
+;; needed in most cases. CONFIG-FILE defaults to "~/.fvwm2rc". This is
+;; right in most cases, and further, most modules totally ignore this
+;; argument. CONFIG-INFO is a list of configuration parameter strings.
+;; It defaults to the registered configuration for this module type,
+;; so in most cases it will be more convenient not to use it. However,
+;; in some cases it may be desirable to launch multiple instances of
+;; an fvwm module with different configurations; this argument makes
+;; it easy to do so. An fvwm2-module object is returned.
+;;
+;;   kill-fvwm2-module FMOD
+;; Kill the module represented by the fvwm2-module object FMOD.
+;;
+;;   kill-all-fvwm2-modules
+;; Kill all running fvwm2 modules.
+;;
+;;   kill-fvwm2-modules-by-name MODULE-NAME
+;; Kill all fvwm2 modules of type MODULE-NAME
 ;;
 ;;
-;; This has only been tested so far with the FvwmPager module. Other modules
-;; should work if all the commands they use are supported by the fvwm-eval
-;; module (q.v.).
+;; Here is an example of using the FvwmPager in a scwmrc:
 ;;
+;; ;; Make sure the right place is in the module path. Note: the
+;; ;; directory listed here is for example purposes only, it is actually
+;; ;; in the default module path.
+;; (set! fvwm2-module-path (append fvwm2-module-path '("/usr/lib/X11/fvwm2")))
+;; ;; Register the module configuration for the pager.
+;; (register-fvwm2-module-config "FvwmPager"
+;;  			         "*FvwmPagerBack grey76"
+;;			         "*FvwmPagerFore black"
+;;			         "*FvwmPagerHilight navyblue"
+;;			         "*FvwmPagerFont none"
+;;			         "*FvwmPagerDeskTopScale 40"
+;;			         "*FvwmPagerLabel 0 Top"
+;;			         "*FvwmPagerLabel 1 Bottom"
+;;			         "*FvwmPagerSmallFont 5x8")
+;;
+;; ;; Actually run the pager. Make it manage desktops 0-2. Save the
+;; ;; module object in a variable so we can kill this specific pager
+;; ;; later.
+;; (define fvwm2-pager (run-fvwm2-module "FvwmPager" '("0" "2")))
+;;
+;; ;; To later shut down the module, use kill-fvwm2-module:
+;;   (kill-fvwm2-module fvwm2-pager)
+;;
+;; ;; To kill all "FvwmPager" modules:
+;;   (kill-fvwm2-modules-by-name "FvwmPager")
+;;
+;; This support is in progress. It may or may not work with particular
+;; modules, depending on wether all the commands they use are
+;; supported by the fvwm-eval module (q.v.).
+;;
+;;
+
 
 ;; FIXMS: Maybe scwm should always ignore sigpipes?
 (sigaction SIGPIPE SIG_IGN)
@@ -190,7 +249,7 @@
     (send-win-string M_ICON_NAME (window-icon-title win))
     
     (let* ((wi (window-icon win))
-	   (wif (if wi (image-property window-icon 'filename) #f)))
+	   (wif (if wi (image-property wi 'filename) #f)))
       (if (and wif
 	       (not (string=? wif "FromApp"))
 	       (not (string=? wif "FromAppBitmap")))
@@ -276,126 +335,190 @@
 
 (add-hook! broadcast-mini-icon-hook module-broadcast-mini-icon)
 
-(define*-public (run-fvwm-module module-file config-file config-info
-				#&optional (other-args '()))
-  (let* ((from-module-pipe (pipe))
-	 (from-module-read (car from-module-pipe))
-	 (from-module-write (cdr from-module-pipe))
-	 (to-module-pipe (pipe))
-	 (to-module-read (car to-module-pipe))
-	 (to-module-write (cdr to-module-pipe))
-	 (input-hook-handle #f)
-	 ;; FIXMS: should use a struct, not a list.
-	 (fmod (list to-module-write 0
-		         (lambda ()
-			   (fvwm2-module-send-config-info 
-			    config-info to-module-write))
-		         (lambda ()
-			   (fvwm2-module-send-window-list
-			    to-module-write))
-			 #f))
-	 (pid (primitive-fork)))
+;;; FIXMS: The next two should be more generally available!
+(define (filename-is-full-path? fname)
+  (and (> (string-length fname) 0)
+       (or (char=? (string-ref fname 0) #\/)
+	   (and (char=? (string-ref fname 0) #\.)
+		(or
+		 (and (> (string-length fname 1))
+		      (char=? (string-ref fname 1) #\/))
+		 (and (> (string-length fname 2))
+		      (char=? (string-ref fname 1) #\.)
+		      (char=? (string-ref fname 2) #\/)))))))
+			 
+(define (find-file-in-path fname path)
+  (if (filename-is-full-path? fname)
+      (if (and (file-exists? fname)
+	       (not (file-is-directory? fname)))
+	  fname
+	  #f)
+      (or-map (lambda (prefix)
+		(let ((fp (string-append prefix "/" fname)))
+		  (if (and (file-exists? fp)
+			   (not (file-is-directory? fp)))
+		      fp
+		      #f))) 
+	      path)))
 
-    (append! fmod
-	     (list (lambda ()
-		     (cond 
-		      ((list-ref fmod 4)
-		       (remove-active-module! fmod)
-		       (catch #t
-			      (lambda ()
-				(close-port to-module-write)
-				(close-port from-module-read))
-			      (lambda args 
-				args))
-		       (remove-input-hook! input-hook-handle)
-		       (list-set! fmod 4 #f))))))
+(define fvwm2-module-config-hash (make-hash-table 5))
 
-    ;; actually, scwm waits on its children so it should be no problem.
-    ;; $SIG{'CHLD'} = sub { wait };
-    ;; Need to look up how to provide sigchld handler
-    (cond
-     ((= pid 0)
-      ;; child process
-      (close-port to-module-write)
-      (close-port from-module-read)
-      (let ((write-fd (number->string (fileno from-module-write)))
-	    (read-fd (number->string (fileno to-module-read))))
-	(cond
-	 ((file-exists? module-file)
-	  ;;	(display (string-append 
-	  ;;		  "child: " module-file " " write-fd " " read-fd " " config-file 
-	  ;;		  " " app-window " " context " " (apply string-append
-	  ;;	(map (lambda (x) (string-apppend " " x)) other-args) "\n") 
-	  ;;	(current-output-port))
-	  (catch #t
-		 (lambda () (apply execl module-file module-file write-fd 
-				   read-fd config-file 
-				   app-window context other-args))
-		 (lambda args #t)))))
-      (display "Exec failed.\n")
-      (close-port from-module-write)
-      (close-port to-module-read)
-      (exit 0)))
+;;;; Public interface
 
-    ;; parent process
+(define-public (get-fvwm2-module-config module-type)
+  (cond 
+   ((hash-ref fvwm2-module-config-hash module-type) => id)
+   (else '())))
 
-    (close-port to-module-read)
-    (close-port from-module-write)
+(define-public (register-fvwm2-module-config module-type . args)
+  (hash-set! fvwm2-module-config-hash module-type args))
 
-    ;; mark fd-s close-on-exec so other processes won't inherit them
-    (fcntl to-module-write F_SETFD 1)
-    (fcntl from-module-read F_SETFD 1)
+(define-public (append-fvwm2-module-config module-type . args)
+  (hash-set! fvwm2-module-config-hash module-type 
+	     (append (get-fvwm2-module-config module-type) args)))
 
-    ;; FIXGJB: set o_nonblock for TO_MODULE_WR
-    ;; GJBFIX: why is this necessary?
-    ;; (fctl to-module-write F_SETFL O_NONBLOCK)
+(define-public (clear-fvwm2-module-config module-type)
+  (hash-remove! fvwm2-module-config-hash module-type))
 
-    (add-active-module! fmod)
-    (list-set! fmod 4 #t)
 
-    (letrec 
-	((packet-handler
-	  (lambda ()
-	    (catch #t
-		   (lambda ()
-		     (let* ((packet (fvwm2-module-read-packet 
-				     from-module-read))
-			    (window-id (car packet))
-			    (command (caddr packet)))	
-	      
-		       ;; (display "packet: ")
-		       ;; (write packet)
-		       ;; (newline)
-              (catch #t
-                     (lambda()
-                       (if (= window-id 0)
-                         (eval-fvwm-command command fmod)
-                         (eval-fvwm-command command
-                                            fmod
-                                            (id->window window-id))))
-                     (lambda args (display "Error evaling packet: ")
-                                  (write packet)
-                                  (newline))
-                     )
-		       (if (not (list-ref fmod 4))
-			   (remove-input-hook! input-hook-handle))))
-		   (lambda args
-		     (display "scwm: Error communicating with module ")
-		     (write fmod)
-		     (display ";\n terminating connection.\n")
-     		     (kill-fvwm-module fmod))))))
+(define-public fvwm2-module-path '("/usr/lib/X11/fvwm2"
+				   "/usr/local/lib/X11/fvwm2"))
+
+
+(define*-public (run-fvwm2-module module-name #&optional
+				  (other-args '())	       
+				  (config-file "~/.fvwm2rc")
+				  (config-info
+				   (get-fvwm2-module-config
+				    (basename module-name))))
+  (let ((module-file (find-file-in-path module-name
+					fvwm2-module-path)))
+    (if module-file
+	(let* ((from-module-pipe (pipe))
+	       (from-module-read (car from-module-pipe))
+	       (from-module-write (cdr from-module-pipe))
+	       (to-module-pipe (pipe))
+	       (to-module-read (car to-module-pipe))
+	       (to-module-write (cdr to-module-pipe))
+	       (input-hook-handle #f)
+	       ;; FIXMS: should use a struct, not a list.
+	       (fmod (list to-module-write 0
+			   (lambda ()
+			     (fvwm2-module-send-config-info 
+			      config-info to-module-write))
+			   (lambda ()
+			     (fvwm2-module-send-window-list
+			      to-module-write))
+			   #f))
+	       (pid (primitive-fork)))
+	  
+	  (append! fmod
+		   (list (lambda ()
+			   (cond 
+			    ((list-ref fmod 4)
+			     (remove-active-module! fmod)
+			     (catch #t
+				    (lambda ()
+				      (close-port to-module-write)
+				      (close-port from-module-read))
+				    (lambda args 
+				      args))
+			     (remove-input-hook! input-hook-handle)
+			     (list-set! fmod 4 #f))))))
+	  (append! fmod (basename module-name))
+
+	  ;; actually, scwm waits on its children so it should be no problem.
+	  ;; $SIG{'CHLD'} = sub { wait };
+	  ;; Need to look up how to provide sigchld handler
+	  (cond
+	   ((= pid 0)
+	    ;; child process
+	    (close-port to-module-write)
+	    (close-port from-module-read)
+	    (let ((write-fd (number->string (fileno from-module-write)))
+		  (read-fd (number->string (fileno to-module-read))))
+	      ;;	(display (string-append 
+	      ;;  "child: " module-file " " write-fd " " read-fd " " 
+	      ;;  config-file " " app-window " " context " " 
+	      ;; (apply string-append
+	      ;;	(map (lambda (x) (string-apppend " " x)) 
+	      ;; other-args) "\n") 
+	      ;;	(current-output-port))
+	      (catch #t
+		     (lambda () (apply execl module-file module-file 
+				       write-fd read-fd config-file 
+				       app-window context other-args))
+		     (lambda args #t)))
+	    (display "Exec failed.\n")
+	    (close-port from-module-write)
+	    (close-port to-module-read)
+	    (exit 0)))
+	  
+	  ;; parent process
+	  
+	  (close-port to-module-read)
+	  (close-port from-module-write)
+	  
+	  ;; mark fd-s close-on-exec so other processes won't inherit them
+	  (fcntl to-module-write F_SETFD 1)
+	  (fcntl from-module-read F_SETFD 1)
+	  
+	  ;; FIXGJB: set o_nonblock for TO_MODULE_WR
+	  ;; GJBFIX: why is this necessary?
+	  ;; (fctl to-module-write F_SETFL O_NONBLOCK)
+	  
+	  (add-active-module! fmod)
+	  (list-set! fmod 4 #t)
+	  
+	  (letrec 
+	      ((packet-handler
+		(lambda ()
+		  (catch #t
+			 (lambda ()
+			   (let* ((packet (fvwm2-module-read-packet 
+					   from-module-read))
+				  (window-id (car packet))
+				  (command (caddr packet)))	
+			     
+			     ;; (display "packet: ")
+			     ;; (write packet)
+			     ;; (newline)
+			     (catch #t
+				    (lambda()
+				      (if (= window-id 0)
+					  (eval-fvwm-command command fmod)
+					  (eval-fvwm-command command
+							     fmod
+							     (id->window 
+							      window-id))))
+				    (lambda args (display 
+						  "Error evaling packet: ")
+					    (write packet)
+					    (newline))
+				    )
+			     (if (not (list-ref fmod 4))
+				 (remove-input-hook! input-hook-handle))))
+			 (lambda args
+			   (display "scwm: Error communicating with module ")
+			   (write fmod)
+			   (display ";\n terminating connection.\n")
+			   (kill-fvwm-module fmod))))))
+	    
+	    (set! input-hook-handle (add-input-hook! from-module-read 
+						     packet-handler))
+	    fmod))
+	(error (string-append "Unable to run fvwm2 module " module-name)))))
+
       
-	  (set! input-hook-handle (add-input-hook! from-module-read 
-						   packet-handler))
-	  fmod)))
-
-(define-public (kill-fvwm-module fmod)
+(define-public (kill-fvwm2-module fmod)
   ((list-ref fmod 5)))
+       
+(define-public (kill-all-fvwm2-modules)
+  (map kill-fvwm2-module active-modules))
 
-(define-public (kill-all-fvwm-modules)
-  (map (lambda (fmod)
-	 (kill-fvwm-module fmod))
-       active-modules))
+(define-public (kill-fvwm2-modules-by-name module-name)
+  (map kill-fvwm2-module (pick (lambda (fmod)  
+				(string=? (list-ref fmod 6) module-name)) 
+				active-modules)))
 
-(add-hook! shutdown-hook kill-all-fvwm-modules)
-
+(add-hook! shutdown-hook kill-all-fvwm2-modules)
