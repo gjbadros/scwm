76bd9ec3452346afb67ed4dcfd676fe28ce4df65
Greg J. Badros <gjbadros@sourceforge.net>
1998-07-24 01:16:06 +0000

    For testing some of the interactions between menu font and the resize/move window position/size display

---
+++ scheme/tests/menu-font.scm
@@ -0,0 +1,13 @@
+;; $Id$
+
+;; see if interactive move/size window reconfigures itself after this
+(set-menu-font! (make-font "*helvetica*medium-r*12*"))
+
+(set-menu-font! (make-font "*helvetica*medium-r*18*"))
+
+(set-menu-font! (make-font "*helvetica*medium-r*24*"))
+
+
+;;; weird interactions with this?
+(menu-style #:fg "black" #:bg "gray80" #:stipple "grey35" 
+	    #:font menu-font #:mwm #f)
