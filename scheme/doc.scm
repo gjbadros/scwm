6468c6ba23b4f45ff9af19f32b42f0f8719be76f
Greg J. Badros <gjbadros@sourceforge.net>
1998-07-30 02:22:13 +0000

    Added, so that all profiles can use the module w/o importing flux stuff

---
+++ scheme/doc.scm
@@ -0,0 +1,47 @@
+;;; $Id$
+;;; doc.scm
+;;; (C) 1998 Sam Steingold, Greg J. Badros, and Maciej Stachowiak
+
+
+
+
+(define-module (app scwm doc)
+  :use-module (ice-9 regex)
+  :use-module (app scwm base)
+  :use-module (app scwm optargs))
+
+
+
+(define-public doc-files        ; '("/usr/src/scwm/doc/scwm-procedures.txt")
+  (map (lambda (s) (string-append (scwm-path-prefix) "/share/scwm/" s))
+       '("scwm-procedures.txt" "cassowary_scm-procedures.txt")))
+
+(define*-public (documentation func #&optional (port (current-output-port)))
+  "Print the documentation for the string or symbol.
+Return #t if found anything, #f if no documentation."
+  (let* ((head (string-append
+                "(" (if (string? func) func (symbol->string func))))
+         (len (string-length head))
+         (delim (lambda (st) (and (= 1 (string-length st))
+                                  (char=? (string-ref st 0)  #\np)))))
+    (do ((fl doc-files (cdr fl)) (done #f) (fd #f))
+        ((or (null? fl) done)
+         (if (not done) (write-all #t "No documentation for `" func "'\n"))
+         done)
+;;      (display "trying ") (display (car fl)) (display "...\n")
+      (set! fd (open-input-file (car fl)))
+      (do ((ln (read-line fd) (read-line fd)) (delim-p #f))
+          ((or (eof-object? ln) done) (close-input-port fd))
+        (cond ((delim ln) (set! delim-p #t))
+              ((and delim-p (< len (string-length ln))
+                    (string=? head (substring ln 0 len))
+                    (string-index " )" (string-ref ln len)))
+               (set! done #t)
+               (display ln port) (newline port)
+               (do ((ln (read-line fd) (read-line fd)))
+                   ((delim ln))
+                 (display ln port) (newline port))))))))
+;; For testing...
+;; (documentation "window-position")
+;; (documentation "make-cl-constraint")
+;; (apropos-internal "")
