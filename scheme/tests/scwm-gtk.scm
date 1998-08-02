c7a3e15a4fe19f25a5276bb8ce778a2f3941e625
Greg J. Badros <gjbadros@sourceforge.net>
1998-08-02 23:25:31 +0000

    Added -- code from Maciej's post, combined with a timer hook that I added to make the gtk event loop happy.  We need to find a guile-gtk guru to help Sam with the options stuff.

---
+++ scheme/tests/scwm-gtk.scm
@@ -0,0 +1,22 @@
+;; with the current scwm from CVS, and guile-gtk-0.10 and gtk-1.0.5 from
+;; ftp.gtk.org, typing the following to a scwmrepl results in a very
+;; interesting window:
+;; Maciej --08/02/98
+
+(begin
+  (use-modules (gtk gtk))
+  
+  (define w (gtk-window-new 'toplevel))
+  (define b (gtk-button-new-with-label "Testing guile-gtk under scwm."))
+  (gtk-container-add w b)
+  (gtk-widget-show b)
+  (gtk-widget-show w)
+  (while (not (= 0 (gtk-events-pending))) (gtk-main-iteration))
+  (while (not (= 0 (gtk-events-pending))) (gtk-main-iteration))
+  (while (not (= 0 (gtk-events-pending))) (gtk-main-iteration)))
+
+(define (handle-gtk-events)
+  (while (not (= 0 (gtk-events-pending))) (gtk-main-iteration))
+  (add-timer-hook! 10000 handle-gtk-events))
+
+(define gtk-loop-timer (add-timer-hook! 10000 handle-gtk-events))
