df40f3d773f452e6491af536e6e751e1f28fb47b
Robert Bihlmeyer <robbe@orcus.priv.at>
1998-09-07 13:50:22 +0000

    Added titles to test-cases.

--- scheme/tests/properties.scm
+++ scheme/tests/properties.scm
@@ -2,15 +2,15 @@
 
 ;; atoms
 
-(test-case
+(test-case "X-atom -> string"
  (X-atom->string 1)
  => "PRIMARY")
 
-(test-case
+(test-case "string -> X-atom"
  (string->X-atom "SECONDARY")
  => 2)
 
-(test-case
+(test-case "string -> X-atom -> string"
   (X-atom->string (string->X-atom "WM_FOO"))
   => "WM_FOO")
 
@@ -25,21 +25,20 @@
 				    (eqv? (cadr size) 123)
 				    (wildcard-match? "xlogo" w)))))))
 
-; a string property
-(test-case
+(test-case "Get a string property (WM_NAME)"
  (X-property-get xlogo "WM_NAME")
  => '("xlogo" "STRING" 8))
 
-; a format-32 property
-(test-case
+(test-case "Get a format 32 property (WM_PROTOCOLS)"
  (X-property-get xlogo "WM_PROTOCOLS")
  => `(,(list->vector (list (string->X-atom "WM_DELETE_WINDOW"))) "ATOM" 32))
 
-; likewise, get the icon window id
-(vector-ref (car (X-property-get (current-window-with-focus) "WM_STATE")) 1)
+(test-case "Get the icon window id from WM_STATE"
+  (vector-ref (car (X-property-get (current-window-with-focus) "WM_STATE")) 1)
+  #t)
 
 ; a list of strings
-(test-case
+(test-case "Get a string property containing multiple strings (WM_COMMAND)"
  (separate-fields-discarding-char #\null
 				  (car (X-property-get
 					xlogo
@@ -49,54 +48,52 @@
 
 (delete-window xlogo)
 
-; set a property
-(test-case
+(test-case "Set my own text property"
  (X-property-set! 'root-window "WM_FOO" "Testing ... 1 ... 2")
  #t)
 
-; consume it
-(test-case
+(test-case "Consume my own property"
  (X-property-get 'root-window "WM_FOO" #t)
  => '("Testing ... 1 ... 2" "STRING" 8))
 
-(test-case
+(test-case "Get an already consumed property"
  (X-property-get 'root-window "WM_FOO")
  => #f)
 
-(test-case
+(test-case "Set my own format 16 property"
  (X-property-set! 'root-window "WM_FOO" #(-2 3) "WM_BAR" 16)
  #t)
 
-(test-case
+(test-case "Get my own format 16 property"
  (X-property-get 'root-window "WM_FOO")
  => '(#(-2 3) "WM_BAR" 16))
 
-(test-case
+(test-case "Prepend to my own format 16 property"
  (X-property-set! 'root-window "WM_FOO" #(1) "WM_BAR" 16 'prepend)
  #t)
 
-(test-case
+(test-case "Get the property with prependix"
  (X-property-get 'root-window "WM_FOO" #f)
  => '(#(1 -2 3) "WM_BAR" 16))
 
-(test-case
+(test-case "Append to my own format 16 property"
  (X-property-set! 'root-window "WM_FOO" #(4 -5 6) "WM_BAR" 16 'append)
  #t)
 
-(test-case
+(test-case "Get the property with prependix and appendix"
  (X-property-get 'root-window "WM_FOO" #t)
  => '(#(1 -2 3 4 -5 6) "WM_BAR" 16))
 
 ; error conditions
-(test-case
+(test-case "Set a property with an illegal mode"
  (X-property-set! 'root-window "WM_FOO" #(1) "WM_BAR" 16 'root-window)
  #f)
 
-(test-case
+(test-case "Set a property with value out of format 8 range"
  (X-property-set! 'root-window "WM_FOO" #(500 0) "WM_BAR" 8)
  #f)
 
-(test-case
+(test-case "Set a property with value out of format 16 range"
  (X-property-set! 'root-window "WM_FOO" #(-100000 0) "WM_BAR" 16)
  #f)
 
