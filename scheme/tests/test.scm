f43544582ef8f841ba72f698f419c4ba3fc02d05
Robert Bihlmeyer <robbe@orcus.priv.at>
1998-09-07 13:48:50 +0000

    (test-case): Add TITLE argument.

--- scheme/tests/test.scm
+++ scheme/tests/test.scm
@@ -1,8 +1,11 @@
 ; Test FORM for proper results.
-; (test-case FORM #f) returns #t, if FORM throws an error; #f otherwise.
-; (test-case FORM #t) returns #t, if FORM eval's successfully; #f otherwise.
-; (test-case FORM => RES) returns #t, if FORM eval's to RES; #f otherwise.
-(defmacro-public test-case (FORM . RESULT)
+; TITLE is just for documentation and ignored at the moment.
+; (test-case TITLE FORM #f) returns #t, if FORM throws an error; #f otherwise.
+; (test-case TITLE FORM #t) returns #t, if FORM eval's successfully;
+;				    #f otherwise.
+; (test-case TITLE FORM => RES) returns #t, if FORM eval's to RES;
+;				    #f otherwise.
+(defmacro-public test-case (TITLE FORM . RESULT)
   (catch #t
 	 (lambda ()
 	   (let ((res (eval FORM))
