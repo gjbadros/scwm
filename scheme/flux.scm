6284932293ad47f7f7f08b627b25a7eec800d3c7
Greg J. Badros <gjbadros@sourceforge.net>
1998-09-17 18:07:14 +0000

    * flux.scm: Extend printable-char->keysym-string to be (nearly?) complete -- the special characters now work for me.

--- scheme/flux.scm
+++ scheme/flux.scm
@@ -245,16 +245,60 @@ cases.  See `add-timer-hook!'."
   "Return the keysym string corresponding to a printable character.
 CHAR is a scheme character.  The return value is appropriate for
 use by `send-key-press'.  See also `X-synthetic-send-string'."
-  (let ((charval (char->integer char)))
-    (cond ((char=? char #\space) "space")
-	  ((char=? char #\newline) "Return")
-	  ((char=? char #\cr) "Return")
-	  ((= charval 27) "Escape")
-	  ((< charval 32) (string-append "C-" (make-string 1 (integer->char
-							      (+ 64 charval)))))
-	  (#t (make-string 1 char)))))
-
+  (let ((charval (char->integer char))
+	(char-keysym-alist 
+	 '((#\space . "space")
+	   (#\newline . "Linefeed")
+	   (#\cr . "Return")
+	   (#\esc . "Escape")
+	   (#\bs . "BackSpace")
+	   (#\del . "Delete")
+	   (#\tab . "Tab")
+	   (#\! . "S-1") ;; "exclam"
+	   (#\" . "S-apostrophe") ;; "quotedbl"
+	   (#\# . "S-3") ;; "numbersign"
+	   (#\$ . "S-4") ;; "dollar"
+	   (#\% . "S-5") ;; "percent"
+	   (#\& . "S-7") ;; "ampersand"
+	   (#\' . "apostrophe")
+	   (#\( . "S-9") ;; "parenleft"
+	   (#\) . "S-0") ;; "parenright"
+	   (#\* . "S-8") ;; "asterisk"
+	   (#\+ . "S-equal") ;; "plus"
+	   (#\, . "comma")
+	   (#\- . "minus")
+	   (#\. . "period")
+	   (#\/ . "slash")
+	   (#\: . "S-semicolon") ;; "colon"
+	   (#\; . "semicolon")
+	   (#\< . "less")
+	   (#\= . "equal")
+	   (#\> . "S-period") ;; "greater"
+	   (#\? . "S-slash") ;; "question"
+	   (#\@ . "S-2") ;; "at"
+	   (#\[ . "bracketleft")
+	   (#\\ . "backslash")
+	   (#\] . "bracketright")
+	   (#\^ . "S-6") ;; "caret"
+	   (#\_ . "S-minus") ;; "underscore"
+	   (#\` . "grave")
+	   (#\' . "quoteleft")
+	   (#\{ . "S-bracketleft") ;; "braceleft"
+	   (#\| . "S-backslash") ;; "bar"
+	   (#\} . "S-bracketright") ;; "brackeright"
+	   (#\~ . "S-grave") ;; "asciitilde"
+	   )))
+    (let ((cell (assq char char-keysym-alist)))
+      (cond
+       (cell
+	(cdr cell))
+       ((< charval 32) (string-append "C-" 
+				      (make-string 1 (integer->char
+						      (+ 64 charval)))))
+       (#t (make-string 1 char))))))
 ;; (printable-char->keysym-string "")
+;; (X-synthetic-send-string "!@#$%^&*()_+[]\\{}|;':\",./<>?`~" (get-window))
+
 
 (define*-public (X-synthetic-send-string str #&optional (win (get-window)))
   "Send string STR to WIN via synthetic X events.
