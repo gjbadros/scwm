a3cf4084f615e9a69c07ab8ac011d676326adf76
Greg J. Badros <gjbadros@sourceforge.net>
1998-09-24 19:12:57 +0000

    * style.scm: Go back to using after-new-window-hook instead of before-place-new-window-hook for the window styles.  Todd's patch may have fixed the placement-related bugs in a cleaner way.

--- scheme/style.scm
+++ scheme/style.scm
@@ -111,7 +111,7 @@
 (define window-hint-hooks '())
 
 
-(add-hook! before-place-new-window-hook
+(add-hook! after-new-window-hook
 	   (lambda (win)
 	     (for-each (lambda (hook) 
 			 (hook win))
