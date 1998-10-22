1efd8d442e9097e34b2f45db76270fcb7e1c0c8e
Robert Bihlmeyer <robbe@orcus.priv.at>
1998-10-22 13:49:33 +0000

    * tests/misc.scm: test-caseification of older tests. New tests for `user-name' and `user-home'.

--- scheme/tests/misc.scm
+++ scheme/tests/misc.scm
@@ -1,10 +1,24 @@
 ;; $Id$
 
-(X-cut-buffer-string)
+(test-case "Read out the X cut buffer contents"
+ (X-cut-buffer-string)
+ #t)
 
-(X-property-get 'root-window "CUT_BUFFER1")
+(test-case "Read out the X cut buffer property"
+ (X-property-get 'root-window "CUT_BUFFER1")
+ #t)
 
-(X-rotate-cut-buffers 1)
+(test-case "Rotate the X cut buffers"
+ (X-rotate-cut-buffers 1)
+ #t)
+
+(test-case "Determine the current user's name."
+ (user-name)
+ => (getenv "LOGNAME"))
+
+(test-case "Determine the current user's homedir."
+ (user-home)
+ => (getenv "HOME"))
 
 ;; (flash-window (current-window-with-focus))
 ;;(set-window-highlight-background! "navy")
