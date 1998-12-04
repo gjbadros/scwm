c9542a6371865fb8cb0e25e12cf3f83fd818dfe3
Sam Steingold <sds@goems.com>
1998-12-04 17:42:12 +0000

    * doc.scm (documentation): cleaned up. 	(doc-files): added more files.

--- scheme/doc.scm
+++ scheme/doc.scm
@@ -16,9 +16,8 @@
 ;; FIXGJB: hack to put doc-files in root module
 (define-public doc-files
   (map (lambda (st) (string-append (scwm-path-prefix) "/share/scwm/" st))
-       '("scwm-procedures.txt" "scwm-variables.txt"
-         "cassowary_scm-procedures.txt")))
-
+       '("scwm-procedures.txt" "scwm-variables.txt" "scwm-hooks.txt"
+         "scwm-concepts.txt" "cassowary_scm-procedures.txt")))
 
 (define-public documentation-debug #t)
 
@@ -28,8 +27,8 @@ Return #t if found anything, #f if no documentation."
   (let* ((head (string-append
                 "(" (if (string? func) func (symbol->string func))))
          (len (string-length head))
-         (delim (lambda (st) (and (= 1 (string-length st))
-                                  (char=? (string-ref st 0)  #\np)))))
+         (delim? (lambda (st) (and (= 1 (string-length st))
+                                   (char=? (string-ref st 0) #\np)))))
     (do ((fl doc-files (cdr fl)) (done #f) (fd #f))
         ((or (null? fl) done)
          (if (not done) (write-all port "No documentation for `" func "'\n"))
@@ -38,17 +37,20 @@ Return #t if found anything, #f if no documentation."
       (cond ((file-exists? (car fl))
              (if documentation-debug (display "file exists\n" port))
              (set! fd (open-input-file (car fl)))
-             (do ((ln (read-line fd) (read-line fd)) (delim-p #f))
+             (do ((ln (read-line fd)))
                  ((or (eof-object? ln) done) (close-input-port fd))
-               (cond ((delim ln) (set! delim-p #t))
-                     ((and delim-p (< len (string-length ln))
+               (cond ((and (delim? ln)
+                           (begin (set! ln (read-line fd))
+                                  (not (eof-object? ln)))
+                           (< len (string-length ln))
                            (string=? head (substring ln 0 len))
                            (string-index " )" (string-ref ln len)))
                       (set! done #t)
                       (display ln port) (newline port)
                       (do ((ln (read-line fd) (read-line fd)))
-                          ((delim ln))
-                        (display ln port) (newline port))))))
+                          ((delim? ln))
+                        (display ln port) (newline port)))
+                     ((set! ln (read-line fd))))))
             (documentation-debug (display "file not found\n" port))))))
 
 (define*-public (help obj #&optional (port (current-output-port)))
