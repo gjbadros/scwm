57990086d42c94669cef3b36fc04c877f73109b5
Greg J. Badros <gjbadros@sourceforge.net>
1999-01-20 21:21:40 +0000

    Added xlib-set-drawing-mask! call

--- scheme/tests/drawing.scm
+++ scheme/tests/drawing.scm
@@ -2,9 +2,8 @@
 
 (use-modules (app scwm xlib-drawing))
 
+(xlib-set-drawing-mask! 29)
 (xlib-draw-rectangle! 20 20 40 50)
 (xlib-draw-line! 20 20 40 50)
 (xlib-draw-arc! 20 20 40 50 20 90)
 (xlib-set-line-width! 4)
-
-
