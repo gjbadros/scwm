6c30104577bdaaa0577a7d69324d38c7fcf62785
Greg J. Badros <gjbadros@sourceforge.net>
1999-01-20 18:36:18 +0000

    * kwm-emulation.scm: Some general clean up, try to handle window-list better, and try handling iconifications.

--- scheme/kwm-emulation.scm
+++ scheme/kwm-emulation.scm
@@ -42,6 +42,10 @@
 (define KWM_MODULE_DESKTOP_NAME_CHANGE (string->X-atom "KWM_MODULE_DESKTOP_NAME_CHANGE"))
 (define KWM_MODULE_DESKTOP_NUMBER_CHANGE (string->X-atom "KWM_MODULE_DESKTOP_NUMBER_CHANGE"))
 (define KWM_MODULE_WIN_ADD (string->X-atom "KWM_MODULE_WIN_ADD"))
+(define KWM_MODULE_WIN_REMOVE (string->X-atom "KWM_MODULE_WIN_REMOVE"))
+(define KWM_MODULE_WIN_CHANGE (string->X-atom "KWM_MODULE_WIN_CHANGE"))
+(define KWM_WIN_ICONIFIED (string->X-atom "KWM_WIN_ICONIFIED"))
+(define KWM_WIN_MAXIMIZED (string->X-atom "KWM_WIN_MAXIMIZED"))
 
 
 (define desktop-names (list "Fun1" "Foo2" "Bar3" "Baz4"))
@@ -97,7 +101,7 @@
   (map (lambda (winid) (send-client-message winid atom data))
        kwm-module-windows))
 
-(define (kwm-change-desk new old)
+(define (kwm-change-desk-handler new old)
   (let ((kwm-desk-number (+ 1 new)))
     (kwm-numeric-property-set! "CURRENT_DESKTOP" kwm-desk-number)
     (send-kwm-modules-client-message 
@@ -146,19 +150,50 @@
   (display (X-atom->string atom)) (display " ") 
   (display deleted?) (display "\n"))
 
-(define (kwm-send-window-list)
+(define-public (kwm-send-window-list)
   (map (lambda (win) 
 	 (send-kwm-modules-client-message KWM_MODULE_WIN_ADD (window-id win)))
        (list-all-windows)))
 
-(define (kwm-emulation-initialize)
-  (kwm-numeric-property-set! "RUNNING" 1)
+(define (kwm-after-new-window-handler win)
+  (send-kwm-modules-client-message KWM_MODULE_WIN_ADD (window-id win)))
+
+(define (kwm-change-window-handler win)
+  (send-kwm-modules-client-message KWM_MODULE_WIN_CHANGE (window-id win)))
+
+(define (kwm-remove-window-handler win)
+  (send-kwm-modules-client-message KWM_MODULE_WIN_REMOVE (window-id win)))
+
+(define (kwm-iconify-window-handler win)
+  (X-property-set! win "KWM_WIN_ICONIFIED" 1 "KWM_WIN_ICONIFIED")
+  (kwm-change-window-handler win))
+
+(define (kwm-deiconify-window-handler win)
+  (X-property-set! win "KWM_WIN_ICONIFIED" 1 "KWM_WIN_ICONIFIED")
+  (kwm-change-window-handler win))
+
+(define-public (kwm-emulation-initialize)
   (kwm-numeric-property-set! "NUMBER_OF_DESKTOPS" number-of-desktops)
   (kwm-numeric-property-set! "CURRENT_DESKTOP" (current-desk))
   (kwm-set-desktop-names)
 
+  ;; (reset-hook! X-UnmapNotify-hook)
+  (add-hook! X-UnmapNotify-hook kwm-remove-window-handler)
+
+  ;; (reset-hook! X-DestroyNotify-hook)
+  (add-hook! X-DestroyNotify-hook kwm-remove-window-handler)
+
+  ;; (reset-hook! after-new-window-hook)
+  (add-hook! after-new-window-hook kwm-after-new-window-handler)
+
+  ;; (reset-hook! iconify-hook)
+  (add-hook! iconify-hook kwm-iconify-window-handler)
+
+  ;; (reset-hook! deiconify-hook)
+  (add-hook! deiconify-hook kwm-deiconify-window-handler)
+
   ;; (reset-hook! change-desk-hook)
-  (add-hook! change-desk-hook kwm-change-desk)
+  (add-hook! change-desk-hook kwm-change-desk-handler)
 
   ;; (reset-hook! client-message-hook)
   ;; (add-hook! client-message-hook client-message-debug-handler)
@@ -168,6 +203,27 @@
   ;;(add-hook! X-root-PropertyNotify-hook root-property-notify-debug-handler)
   (add-hook! X-root-PropertyNotify-hook kwm-root-property-notify-handler)
 
+  (kwm-numeric-property-set! "RUNNING" 1)
+
   (kwm-send-window-list))
+
+(define-public (kwm-emulation-reset)
+  (remove-hook! X-UnmapNotify-hook kwm-remove-window-handler)
+
+  (remove-hook! X-DestroyNotify-hook kwm-remove-window-handler)
+
+  (remove-hook! after-new-window-hook kwm-after-new-window-handler)
+
+  (remove-hook! iconify-hook kwm-change-window-handler)
+
+  (remove-hook! deiconify-hook kwm-change-window-handler)
+
+  (remove-hook! change-desk-hook kwm-change-desk-handler)
+
+  ;; (remove-hook! client-message-hook client-message-debug-handler)
+  (remove-hook! client-message-hook kwm-client-message-handler)
+
+  ;;(remove-hook! X-root-PropertyNotify-hook root-property-notify-debug-handler)
+  (remove-hook! X-root-PropertyNotify-hook kwm-root-property-notify-handler))
 
 (kwm-emulation-initialize)
