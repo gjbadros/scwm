e8f11b5be34470abc2df64a372c6003ffcb05be5
Greg J. Badros <gjbadros@sourceforge.net>
1999-01-20 18:45:35 +0000

    Added -- example of xlib-drawing module

---
+++ scheme/tests/drawing.scm
@@ -0,0 +1,10 @@
+;; $Id$
+
+(use-modules (app scwm xlib-drawing))
+
+(xlib-draw-rectangle! 20 20 40 50)
+(xlib-draw-line! 20 20 40 50)
+(xlib-draw-arc! 20 20 40 50 20 90)
+(xlib-set-line-width! 4)
+
+
