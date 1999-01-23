56a77f99e8149f49891195d93b6cd21ab6918ee6
Greg J. Badros <gjbadros@sourceforge.net>
1999-01-23 01:15:16 +0000

    * constraints.scm: Added -- top level module for constraint support in Scwm

---
+++ scheme/constraints.scm
@@ -0,0 +1,22 @@
+;; $Id$
+;; (C) 1999 Greg J. Badros
+
+(define-module (app scwm constraints)
+  :use-module (app scwm ui-constraints-buttons)
+  :use-module (app scwm ui-constraints)
+  :use-module (app scwm modifier-key-bindings))
+
+
+;; (use-modules (app scwm constraints))
+;; (use-modules (app scwm ui-constraints-buttons))
+
+(start-ui-constraints-buttons) 
+
+(bind-three-modifier-key-events 
+ XKM_CONTROL_L  XKM_ALT_L  XKM_SHIFT_L
+;; (37 . 4) (115 . 16) (50 . 1)
+ draw-all-constraints
+ undraw-all-constraints)
+
+(window-style (resource-match?? "ScwmUIConstraintsButtons") 
+	      #:use-style desk-widget-on-top-no-titlebar)
