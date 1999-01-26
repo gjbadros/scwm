edce64666a291f8ad4ad61e74223e99a5fa86294
Greg J. Badros <gjbadros@sourceforge.net>
1999-01-26 23:46:45 +0000

    * simple-constraints.scm: Added synopsis doc comments, declare variable v.

--- scheme/simple-constraints.scm
+++ scheme/simple-constraints.scm
@@ -7,7 +7,10 @@
 (define-public solver (make-cl-solver))
 (scwm-set-master-solver! solver)
 
+(define-public v (make-cl-variable "v"))
+
 (define*-public (keep-tops-even w1 w2 #&optional (enable? #f))
+  "Keep W1 and W2 aligned at the top."
   (let ((w1-y (window-clv-yt w1))
         (w2-y (window-clv-yt w2)))
     (let ((cn (make-cl-constraint w1-y = w2-y)))
@@ -15,6 +18,7 @@
       cn)))
 
 (define*-public (keep-lefts-even w1 w2 #&optional (enable? #f))
+  "Keep W1 and W2 aligned at the left."
   (let ((w1-x (window-clv-xl w1))
         (w2-x (window-clv-xl w2)))
     (let ((cn (make-cl-constraint w1-x = w2-x)))
@@ -22,6 +26,7 @@
       cn)))
 
 (define*-public (keep-bottoms-even w1 w2 #&optional (enable? #f))
+  "Keep W1 and W2 aligned at the bottom."
   (let ((w1-y (window-clv-yb w1))
         (w2-y (window-clv-yb w2)))
     (let ((cn (make-cl-constraint w1-y = w2-y)))
@@ -29,6 +34,7 @@
       cn)))
 
 (define*-public (keep-rights-even w1 w2 #&optional (enable? #f))
+  "Keep W1 and W2 aligned at the right."
   (let ((w1-x (window-clv-xr w1))
         (w2-x (window-clv-xr w2)))
     (let ((cn (make-cl-constraint w1-x = w2-x)))
@@ -36,6 +42,7 @@
       cn)))
 
 (define*-public (keep-to-left-of w1 w2 #&optional (enable? #f))
+  "Keep W1 to the left of W2."
   (let ((w1-xr (window-clv-xr w1))
         (w2-xl (window-clv-xl w2)))
     (let ((cn (make-cl-constraint w1-xr <= w2-xl)))
@@ -43,6 +50,7 @@
       cn)))
 
 (define*-public (keep-above w1 w2 #&optional (enable? #f))
+  "Keep W1 wholly above W2."
   (let ((w1-yb (window-clv-yb w1))
         (w2-yt (window-clv-yt w2)))
     (let ((cn (make-cl-constraint w1-yb <= w2-yt)))
@@ -50,24 +58,31 @@
       cn)))
 
 (define*-public (keep-top-at-v w1 #&optional (enable? #f))
+  "Keep W1's top at cl-variable `v's pixel value.
+Use `cl-set-solver-var' to change `v'."
   (let ((w1-y (window-clv-yt w1)))
     (let ((cn (make-cl-constraint w1-y = v)))
       (and enable? (cl-add-constraint solver cn))
       cn)))
 
 (define*-public (keep-left-at-v w1 #&optional (enable? #f))
+  "Keep W1's left at cl-variable `v's pixel value.
+Use `cl-set-solver-var' to change `v'."
   (let ((w1-x (window-clv-xl w1)))
     (let ((cn (make-cl-constraint w1-x = v)))
       (and enable? (cl-add-constraint solver cn))
       cn)))
 
 (define*-public (keep-right-at-v w1 #&optional (enable? #f))
+  "Keep W1's right at cl-variable `v's pixel value.
+Use `cl-set-solver-var' to change `v'."
   (let ((w1-x (window-clv-xr w1)))
     (let ((cn (make-cl-constraint w1-x = v)))
       (and enable? (cl-add-constraint solver cn))
       cn)))
 
 (define*-public (keep-full-width w1 w2 #&optional (enable? #f))
+  "Keep W1 and W2 sum of widths equal to the display's width."
   (let ((w1-width (window-clv-width w1))
         (w2-width (window-clv-width w2)))
     (let ((cn (make-cl-constraint (cl-plus w1-width w2-width) = 1152)))
@@ -75,6 +90,7 @@
       cn)))
 
 (define*-public (keep-full-height w1 w2 #&optional (enable? #f))
+  "Keep W1 and W2 sum of heights equal to the display's height."
   (let ((w1-height (window-clv-height w1))
         (w2-height (window-clv-height w2)))
     (let ((cn (make-cl-constraint (cl-plus w1-height w2-height) = 900)))
@@ -82,6 +98,7 @@
       cn)))
 
 (define*-public (keep-adjacent-horizontal w1 w2 #&optional (enable? #f))
+  "Keep W1's right edge aligned with W2's left edge."
   (let ((w1-xr (window-clv-xr w1))
         (w2-xl (window-clv-xl w2)))
     (let ((cn (make-cl-constraint w1-xr = w2-xl)))
@@ -89,6 +106,7 @@
       cn)))
 
 (define*-public (keep-adjacent-vertical w1 w2 #&optional (enable? #f))
+  "Keep W1's bottom edge aligned with W2's top edge."
   (let ((w1-yb (window-clv-yb w1))
         (w2-yt (window-clv-yt w2)))
     (let ((cn (make-cl-constraint w1-yb = w2-yt)))
@@ -96,24 +114,28 @@
       cn)))
 
 (define*-public (keep-at-left-edge w #&optional (enable? #f))
+  "Keep W's left edge at the left edge of the desktop."
   (let ((w-xl (window-clv-xl w)))
     (let ((cn (make-cl-constraint w-xl = 0 cls-strong .1)))
       (and enable? (cl-add-constraint solver cn))
       cn)))
 
 (define*-public (keep-at-top-edge w #&optional (enable? #f))
+  "Keep W's top edge at the top edge of the desktop."
   (let ((w-yt (window-clv-yt w)))
     (let ((cn (make-cl-constraint w-yt = 0)))
       (and enable? (cl-add-constraint solver cn))
       cn)))
 
 (define*-public (keep-at-right-edge w #&optional (enable? #f))
+  "Keep W's right edge at the right edge of the desktop."
   (let ((w-xr (window-clv-xr w)))
     (let ((cn (make-cl-constraint w-xr = 1152 cls-strong .1)))
       (and enable? (cl-add-constraint solver cn))
       cn)))
 
 (define*-public (keep-constant-width w width #&optional (enable? #f))
+  "Keep W's width at WIDTH."
   (let ((w-width (window-clv-width w)))
     (let ((cn (make-cl-constraint w-width = width cls-strong .5)))
       (and enable? (cl-add-constraint solver cn))
@@ -122,12 +144,14 @@
 ;; returns the stay constraints, perhaps to be attached to 
 ;; the window object
 (define*-public (keep-constant-size w #&optional (enable? #f))
+  "Keep W's size from changing."
   (let ((cnw (make-cl-stay-constraint (window-clv-width w) cls-strong 2))
         (cnh (make-cl-stay-constraint (window-clv-height w) cls-strong 2)))
     (and enable? (cl-add-constraint solver cnw cnh))
     (list cnw cnh)))
 
 (define-public (cl-set-solver-var s clv value)
+  "Set solver S's variable CLV to value VALUE."
   (cl-add-editvar s clv)
   (cl-begin-edit s)
   (cl-suggest-value s clv value)
