9b24a36ad2eac29758df97b9bc8c6fffa54330e1
Greg J. Badros <gjbadros@sourceforge.net>
1999-02-09 23:14:15 +0000

    Change the drawing mask for 32 bit screens

--- scheme/tests/drawing.scm
+++ scheme/tests/drawing.scm
@@ -2,7 +2,7 @@
 
 (use-modules (app scwm xlib-drawing))
 
-(xlib-set-drawing-mask! 29)
+(xlib-set-drawing-mask! 2147483647)  ;; 2^31-1, nice for 32 bit displays
 (xlib-set-line-width! 4)
 (xlib-draw-rectangle! '(20 . 20) 40 50)
 (xlib-draw-line! '(20 . 20) '(40 . 50))
