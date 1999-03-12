b70dd0065e94a9d62974e7a24305875d90f9ec2c
Greg J. Badros <gjbadros@sourceforge.net>
1999-03-12 08:53:07 +0000

    *** empty log message ***

--- scheme/tests/rename-with-string-prompt.scm
+++ scheme/tests/rename-with-string-prompt.scm
@@ -2,7 +2,11 @@
 ;;; By Greg J. Badros, --01/18/99 gjb
 
 (use-modules (app scwm string-prompt))
+(use-modules (gtk gtk))
 
-(let ((w (select-window-interactively "choose window to rename")))
+(define* (rename-window-interactively #&optional (win (get-window)))
   (string-prompt "Rename to: " (lambda (new-name)
-				 (set-window-title! w new-name))))
+				 (set-window-title! win new-name))
+		 "Rename-window"))
+
+(rename-window-interactively)
