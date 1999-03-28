e47e2e13a5fa7d1d143250bc359a88a315af34bf
Greg J. Badros <gjbadros@sourceforge.net>
1999-03-28 06:02:06 +0000

    Added -- some example code for new preferences stuff.

---
+++ scheme/tests/prefs.scm
@@ -0,0 +1,25 @@
+;; $Id$
+
+(use-modules (app scwm defoption)
+	     (app scwm primopts)
+	     (app scwm preferences))
+
+scwm-options
+
+(scwm-option-get *edge-y-scroll*)
+(scwm-option-get *desk-width*)
+
+(scwm-option-set! *desk-width* 2)
+(scwm-option-set! *desk-height* 2)
+
+(popup-option-menu '*desk-width*)
+(popup-option-menu '*desk-height*)
+(popup-option-menu '*auto-raise*)
+
+(gui-set '*auto-raise*)
+(gui-set '*theme-path*)
+
+;; (popup-menu (scwm-options-menu) #t)
+;; *auto-raise*
+;; *edge-y-scroll*
+;; *theme-path*
