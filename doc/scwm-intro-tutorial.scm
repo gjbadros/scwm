511a4cc015a78cad96eff7daa469468dc7331660
Greg J. Badros <gjbadros@sourceforge.net>
1999-04-12 04:04:55 +0000

    Updated to match current state of Scwm, added a bit more text, improved a bit.  Added a skeletal outline for a couple of more sections, though not a whole lot of new text.

--- doc/scwm-intro-tutorial.scm
+++ doc/scwm-intro-tutorial.scm
@@ -16,7 +16,9 @@
 ;; Your .scwmrc is just a sequence of SEXPs which 
 ;; are used to initially configure Scwm to your liking,
 ;; similar to the way you configure other WMs with their
-;; .foowmrc files.
+;; .foowmrc files.  ';' is the comment character in
+;; Scheme, and all text from a semicolon to the end of
+;; line is ignored in your scwmrc and in Scheme code.
 
 ;; But, the scheme interpreter is a *lot* more powerful
 ;; than the static configuration language that other
@@ -150,13 +152,18 @@
 
 ;; Thus "(+ 1 2)" is evaluated by applying the procedure "+" (a 
 ;; built-in primitive procedure) to the arguments "1" and "2".
-;; This can be written more explicitly in Scheme using the
+;; This can be written more indirectly in Scheme using the
 ;; procedure `apply'.  
 
 (apply + (list 1 2))
 
+;; Scheme's `apply' takes two arguments: the procedure to invoke,
+;; and a list of the arguments that the procedure is to receive.
+;; We will see later how useful it is to have procedures which
+;; can take other procedures as arguments.  
+
 ;; In this last example, I used the procedure `list' which just
-;; returns its arguments in a list.  So:
+;; returns its evaluated arguments in a list.  So:
 
 (list 1 2 (+ 1 2))
 
@@ -164,30 +171,72 @@
 ;; all of the arguments before invoking a procedure on those
 ;; arguments.  Thus, in the above, the `+' procedure finishes
 ;; executing and evaluates to a 3 before the `list' procedure
-;; is invoked on the arguments "1" "2" and "3".
+;; is invoked on the arguments "1" "2" and "3" as numbers.
+;; This is exceedingly important to understand, so I'll say it
+;; again:
+
+;; Scheme evaluates all of the arguments before invoking a 
+;; procedure on those arguments.
+
+;; This behaviour is fundamental to the eager evaluation that
+;; Scheme uses.  It is the same rule as in C and most of the
+;; other languages you might be familiar with.  However, since
+;; Scheme uses functions for its work much more pervasively,
+;; it is more important to get this clear in your head.  In
+;; particular, this results in an initially unintuitive inside-out
+;; evaluation.  Be sure you understand the order of evaluation
+;; noted below:
+
+  (+ (* 2 5) (- 3 (/ 4 2)))
+;;                ^^^^^^^ evals to 2
+;;   ^^^^^^^ ^^^^^^^^^^^^ evals to 10 and to 1
+;;                        (in an unspecified order, though
+;;                         it's left->right in guile scheme)
+;;^^^^^^^^^^^^^^^^^^^^^^^^^ evals to 11
+
 
 ;; Though procedures evaluate their arguments,
 ;; two other scheme constructs, special forms and macros,
 ;; permit control over how their arguments are evaluated.
-;; We'll learn more about them later.
+;; `quote' is the name of one special form that explicitly
+;; does not evaluate its argument-- it just returns it
+;; unevaluated, and it will remain unevaluated until
+;; something explicitly evaluates the return value.
+;; Consider:
 
-;; Sometimes we want to suppress evaluation of a list entirely.
-;; Scheme provides a special syntax, the "'" character (single forward
-;; quote, or tic), to do this.  Evaluate each of the two below SEXPs
-;; and compare:
+(quote (+ 1 2))
+;; returns the list (+ 1 2)
+
+;; The argument to `quote' is the list (+ 1 2).  If `quote' were a
+;; procedure, this would be evaluated before running the quote
+;; procedure, and the procedure would get 3 as its single argument.
+;; However, as just mentioned, `quote' is *not* a procedure -- it is a
+;; special form.  Thus, the scheme system does *not* evaluate the
+;; arguments of special forms; instead it lets the form decide how and
+;; when (and even if) to evaluate its arguments.  `quote' is
+;; especially simple--it simply returns its argument unevaluated.
+
+;; This is exceptionally useful, as we often want to suppress the
+;; evaluation of a expression.  It is so useful, in fact, that Scheme
+;; provides a special syntax, the "'" character (single forward quote,
+;; or tic), to do this.  Evaluate each of the two below SEXPs and
+;; compare:
 
 (+ 1 2)
 ;; evaluates and gives us "3"
 
 '(+ 1 2)
 ;; "'" suppresses the evaluation of the list, and the
-;; result is the list containing 3 objects, "(+ 1 2)"
+;; result is the list containing 3 objects, "(+ 1 2)" (the
+;; same as when we used `quote' explicitly above).  In
+;; general "'foo" is the same as "(quote foo)", but just
+;; easier to write.  
 
-
+;; The Scheme interpreter that Scwm embeds is called "Guile Scheme"
+;; that is part of the GNU project.  See:
 
-;; Scwm uses a Scheme interpreter called "Guile Scheme" that
-;; is part of the GNU project.  See:
 ;; http://www.red-bean.com/guile
+
 ;; Guile Scheme provides all of the standard scheme functionality
 ;; of R4RS, and if you have the R4RS Scheme info pages installed
 ;; on your system, you can browse that documentation using
@@ -202,8 +251,8 @@
 
 ;; Finally, Scwm also implements hundreds of new window-manager
 ;; related "primitives" (procedures written in Scheme coding providing
-;; fundamental new capabilities) and procedures [a primitive is a
-;; procedure, but not necessarily vice-versa].  The Emacs scwm-mode
+;; fundamental new capabilities) and procedures (a primitive is a
+;; procedure, but not necessarily vice-versa).  The Emacs scwm-mode
 ;; provides an easy way to access the documentation for these
 ;; procedures:  "C-h C-s" is bound to "scwm-documentation".
 ;; Try it below to find out more about the Scwm procedure below:
@@ -227,51 +276,52 @@ select-window-interactively
 ;; The new Emacs window should look something like this:
 
 
-;;;;  SCWM documentation for `select-window-interactively':
+;;;;  Scwm documentation for `select-window-interactively':
 ;;;;  
 ;;;;   value:
 ;;;;  
-;;;;   #<primitive-procedure select-window-interactively>
+;;;;   #<procedure select-window-interactively lambda*:G17>
 ;;;;  
 ;;;;   documentation:
 ;;;;  
-;;;;  (select-window-interactively  #&optional msg)
-;;;;  - Built-in Primitive
-;;;;  Returns a window selected interactively while displaying MSG.
-;;;;  Returns #f if no window was selected. Display no message if MSG not given.
-;;;;  [From scwm/window.c:809]
+;;;;  (select-window-interactively  #&optional (msg #f) (message-window #f))
+;;;;  - 
+;;;; Return an interactively-selected window after prompting (optionally) with MSG.
+;;;; If given, use message window MESSAGE-WINDOW to display the message, otherwise create
+;;;; a new message window.
+;;;; [From scheme/base.scm:615]
 
 ;; The most important lines are those following the "documentation:"
-;; heading.  The "(select-window-interactively  #&optional msg)"
-;; shows the arguments that the procedure takes.  According to this,
-;; `select-window-interactively' takes a single optional argument,
-;; a "msg".  The description will describe further the purpose of
-;; the argument, and it tells us that the MSG given is displayed
-;; while permitting the user to interactively select a window.
-;; The description also tells us what the return value of the
-;; procedure is:  `select-window-interactively' returns the
-;; selected window if one was selected, or otherwise "#f".
+;; heading.  The "(select-window-interactively #&optional (msg #f)
+;; (message-window #f)" shows the arguments that the procedure takes.
+;; According to this, `select-window-interactively' takes two optional
+;; arguments, a "msg" and a "message-window".  The description will
+;; describe further the purpose of the arguments, and it tells us that
+;; the MSG given is displayed while permitting the user to
+;; interactively select a window.  The description also tells us what
+;; the return value of the procedure is: `select-window-interactively'
+;; returns the selected window if one was selected, or otherwise "#f".
 ;; (#f is the printable form of the boolean object that represents
-;;  FALSE;  #t is the analogous object for TRUE.)
+;; FALSE; #t is the analogous object for TRUE.)
 
 ;; Let's try using the `select-window-interactively' procedure.
 ;; Evaluate the below line:
 
 (select-window-interactively "Pick a window")
-;;                                           ^ C-j here
 
 ;; Your X11 cursor (mouse pointer) will change into a circle, and you
-;; should move it and left click on a window.  After doing so, text
-;; similar to #<window 46137358: "xterm"> will be inserted in the
-;; buffer.  The procedure returned a window object, and that window's
-;; printable representation (as text) is the text that was inserted.
-;; For all Scwm objects, the printable representation starts with "#<"
-;; followed by the kind of object.  The remaining information in the
-;; printable representation depends on what type of object it is;
-;; for window objects, it consists of the X11 window identifier number
-;; followed by the string which is the window title.
+;; should move it and click with your left mouse button on a window.
+;; After doing so, text similar to #<window 46137358: "xterm"> will be
+;; inserted in the buffer.  The procedure returned a window object,
+;; and, as usual, that window's printable representation (as text) is
+;; the text that was inserted.  For all Scwm-specific primitive
+;; objects, their printable representation starts with "#<" followed
+;; by a string giving the kind of the object.  The remaining
+;; information in the printable representation depends on what type of
+;; object it is; for window objects, it consists of the X11 window
+;; identifier number followed by the string which is the window title.
 
-;; You should be careful to not use the printable form of an object
+;; You should be careful not to use the printable form of an object
 ;; to find out information about that object, as the printable form
 ;; is subject to change.  There are accessor procedures for all object
 ;; types that will answer any question you might have about an object.
@@ -302,16 +352,16 @@ select-window-interactively
 window
 ;;    ^ C-h C-a 
 
-;; This will popup a new Emacs window (again, different from
-;; Scwm's notion of windows-- an Emacs window still lives inside
-;; the single Emacs frame [Emacs frame == Scwm window]) that
-;; lists all the symbols with "window" in their name.  This
-;; "apropos" functionality is a good way to learn more about
-;; a certain aspect of Scwm.  From the "*Apropos*" buffer,
-;; you can press return on any of the symbols to learn more
-;; about it.  E.g., move down to `window-id' (perhaps use Emacs's
-;; incremental search feature, "C-s window-id", to find the text),
-;; and press return on that line.  Let's use `window-id':
+;; This will popup a new Emacs window (again, different from Scwm's
+;; notion of windows-- an Emacs window still lives inside the single
+;; Emacs frame [Emacs frame == Scwm window]) that lists all the
+;; symbols with "window" in their name.  This "apropos" functionality
+;; is a good way to learn more about a certain aspect of Scwm.  From
+;; the "*Apropos*" buffer, you can press <Enter> or <Return> on any of
+;; the symbols to learn more about it.  E.g., move down to `window-id'
+;; (perhaps use Emacs's incremental search feature, "C-s window-id",
+;; to find the text), and press return on that line.  Let's use
+;; `window-id':
 
 (window-id (select-window-interactively))
 ;;                                       ^ C-j here
@@ -327,13 +377,71 @@ window
 ;; FROM A SHELL:
 ;; xev -id `scwmexec '(window-id (select-window-interactively))'`
 
-;;                 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-;;                 This SEXP gets evaluated
+;;                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+;;                    This SEXP gets evaluated
 ;;
-;;      ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+;;         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 ;;      and we get something like "46137358" here
 ;;      because the printable representation of the return value
 ;;      is replaced via the shell's command substitution mechanism
-;;
-;; Finally, "xev -id 46137358" is run
+
+;; Finally, "xev -id 46137358" is run, and it will display all the X
+;; events that transpire in the window you chose.  The "scwmexec"
+;; command is a very powerful means of having applications and shell
+;; scripts interact with Scwm.  It uses the same communication
+;; mechanism as scwm-mode for Emacs does.  There is a third way to
+;; talk to the Scheme interpreter embedded in Scwm: the "scwmrepl"
+;; command.  REPL stands for Read-Eval-Print-Loop.  As with scwm-mode 
+;; under Emacs, scwmrepl supports completion of Scwm symbols.
+
+;; Scwm gets a lot of leverage from its embedding of a 
+;; complete, general-purpose programming language.  All of
+;; Guile/Scheme's procedures are available for use with Scwm.
+;; For example:
+
+(getenv "HOME")
+
+;; Will return the value of the environment variable "HOME". 
+
+(rename-file "old-name" "new-name")
+
+;; Will rename a file named "old-name" to "new-name".
+
+;; Though neither of these procedures are exclusively related
+;; to window managing, they are useful to have available
+;; from within a window manager, and Scwm inherits the procedures
+;; from its Guile/Scheme interpreter.
+
+
+;;; More about scheme
+
+;; define bindings
+;; set! in contrast with define
+;; lambda for defining procedures (with define & with let)
+;; special syntax for defining procedures
+;; anonymous functions
+;; begin
+;; let, let*
+;; closures
+
+;;; Recursion
+;; cheating
+;; letrec
+
+
+;;; More about Scwm
+;; window styles
+;; event bindings
+;; menus
+;; hooks
+;; preferences support
+
+
+;;; Guile's module system
+;; define-module
+;; use-modules
+;; bindings being shadowed (set! vs. define)
+;; tricks
+
+
 
