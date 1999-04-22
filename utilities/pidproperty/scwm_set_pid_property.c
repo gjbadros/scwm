b259c8d32aa44ad5e9cd27d1532466b3ca32eb8f
Greg J. Badros <gjbadros@sourceforge.net>
1999-04-22 06:50:12 +0000

    * pidproperty/scwm_set_pid_property.c: Factor out some code into a function, and look for SCWM_OTHER_ID environment variable and set the X property to the env var's value, if the var is set in the env.

--- utilities/pidproperty/scwm_set_pid_property.c
+++ utilities/pidproperty/scwm_set_pid_property.c
@@ -4,7 +4,7 @@
  * 20-April-1999
  * See bottom for compilation command, then do:
 
-LD_PRELOAD=/full/path/to/scwm_set_pid_property.so xlogo
+SCWM_OTHER_ID="anArbitraryString" LD_PRELOAD=$exec_prefix/scwm_set_pid_property.so xlogo
 
 and then you can access the PID of the xlogo process from the window
 property SCWM_RUNNING_PID.
@@ -13,28 +13,32 @@ From Scwm, this is as easy as:
 
 (use-scwm-modules xprop-extras)
 (window-pid WINDOW)
+(window-other-id WINDOW)
 
 !!!
  */
 
-#include <X11/X.h>
+/* #define DEBUG */
 
 #include <dlfcn.h>
 #include <X11/X.h>
 #include <X11/Xlib.h>
+#include <X11/Xatom.h>
 #include <unistd.h>
+#include <stdlib.h>
 #ifdef DEBUG
 #include <stdio.h>
 #endif
 
-#define PROPERTY_NAME "SCWM_RUNNING_PID"
+#define PID_PROPERTY_NAME "SCWM_RUNNING_PID"
+#define OTHER_ID_PROPERTY_NAME "SCWM_OTHER_ID"
 
 static void *pfXCreateWindow = NULL;
 static void *pfXCreateSimpleWindow = NULL;
 static void *dlhX11 = NULL;
 pid_t mypid;
-static Atom XA_PID;
 
+void
 _init() {
   unsetenv("LD_PRELOAD");
   dlhX11=dlopen("libX11.so",RTLD_GLOBAL);
@@ -42,6 +46,29 @@ _init() {
   pfXCreateSimpleWindow=dlsym(dlhX11,"XCreateSimpleWindow");
   mypid = getpid();
 }
+
+#ifndef __GNUC__
+#define __inline__ 
+#endif
+
+__inline__
+void SetXProperties(Display *display, Window w)
+{
+  const char *szOtherId = getenv("SCWM_OTHER_ID");
+  Atom XA_PID = XInternAtom(display,PID_PROPERTY_NAME, False);
+  XChangeProperty(display, w,
+                  XA_PID, XA_INTEGER,
+                  32, PropModeReplace,
+                  (unsigned char *) &mypid, 1);
+  if (szOtherId) {
+    Atom XA_OTHER_ID = XInternAtom(display,OTHER_ID_PROPERTY_NAME, False);
+    XChangeProperty(display, w,
+                    XA_OTHER_ID, XA_STRING,
+                    8, PropModeReplace,
+                    (unsigned char *) szOtherId, strlen(szOtherId));
+  }
+}
+
 
 typedef Window (*XCType)(Display *display, Window, int, int, unsigned int,
                          unsigned int, unsigned int, int, unsigned int, Visual *, unsigned
@@ -70,11 +97,7 @@ Window XCreateWindow(Display *display,
 #ifdef DEBUG
     fprintf(stderr,"called XCreateWindow, got %ld\n",w);
 #endif
-    XA_PID = XInternAtom(display,PROPERTY_NAME, False);
-    XChangeProperty(display, w,
-                    XA_PID, 1,
-                    32, PropModeReplace,
-                    (unsigned char *) &mypid, 1);
+    SetXProperties(display,w);
   }
   return w;
 }
@@ -96,11 +119,7 @@ Window XCreateSimpleWindow(Display *display,
 #ifdef DEBUG
     fprintf(stderr,"called XCreateSimpleWindow, got %ld\n",w);
 #endif
-    XA_PID = XInternAtom(display,PROPERTY_NAME, False);
-    XChangeProperty(display, w,
-                    XA_PID, 1,
-                    32, PropModeReplace,
-                    (unsigned char *) &mypid, 1);
+    SetXProperties(display,w);
   }
   return w;
 }
