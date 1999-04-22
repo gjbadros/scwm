a4e0922658fcf459bc112fbb155000484377a635
Greg J. Badros <gjbadros@sourceforge.net>
1999-04-22 10:29:30 +0000

    * run-renamed/Makefile.am, run-renamed/run-renamed.c: Added -- from Ken Pizzini and my discussion on scwm-discuss.  Permits programmatically changing WM_COMMAND of an X application to embed an identifier to later be used to map back to the execution that started the process that mapped the window.  Similar purpose to pidproperty, but different approach. Credit for the approach goes to Robert Bihlmeyer.

---
+++ utilities/run-renamed/run-renamed.c
@@ -0,0 +1,36 @@
+/* $Id$ 
+ * run-renamed.c
+ * (C) 1999 By Ken Pizzini and Greg J. Badros
+ * GPL'd
+ */
+
+#include <unistd.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *r1bindex(char *p, int c)
+{
+  char *r = strrchr(p, c); 
+  return r ? r+1 : p;
+}
+
+int main(int argc, char **argv) 
+{ 
+  if (argc < 1) exit(1);
+
+  if (argc < 2) {
+    fprintf(stderr, "USAGE: %s command-to-run [renamed-to args...]\n", argv[0]); 
+    exit(1);
+  }
+  if (!strcmp(r1bindex(argv[0], '/'), "run-renamed")) { 
+    setenv("RESOURCE_NAME",argv[1],1);
+  } else {
+    char *sz = (char *) malloc(strlen(argv[1]) + strlen(argv[0]) + 12);
+    sprintf(sz,"rename_to=%s %s",argv[1],argv[0]);
+    argv[1] = sz;
+  }
+  execvp(argv[0], argv+1);
+  perror("run-renamed");
+  exit(127); 
+} 
