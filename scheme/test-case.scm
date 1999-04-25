817be6d4097572afee944bcf5478ca03a690c59a
Greg J. Badros <gjbadros@sourceforge.net>
1999-04-25 06:17:16 +0000

    * test-case.scm: Added (from tests/test.scm)

---
+++ scheme/test-case.scm
@@ -0,0 +1,23 @@
+;; $Id$
+
+; Test FORM for proper results.
+; TITLE is just for documentation and ignored at the moment.
+; (test-case TITLE FORM #f) returns #t, if FORM throws an error; #f otherwise.
+; (test-case TITLE FORM #t) returns #t, if FORM eval's successfully;
+;				    #f otherwise.
+; (test-case TITLE FORM => RES) returns #t, if FORM eval's to RES;
+;				    #f otherwise.
+
+(define-module (app scwm test-case))
+
+(defmacro-public test-case (TITLE FORM . RESULT)
+  (catch #t
+	 (lambda ()
+	   (let ((res (eval FORM))
+		 (succeed (car RESULT)))
+	     (cond
+	      ((eq? succeed '=>) (equal? res (eval (cadr RESULT))))
+	      ((eq? succeed #t) #t)
+	      (else #f))))
+	 (lambda (key . args)
+	   (eq? (car RESULT) #f))))
