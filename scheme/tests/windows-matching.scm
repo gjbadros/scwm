6ddb338b52696a296c885756ac8ff69600420378
Greg J. Badros <gjbadros@sourceforge.net>
1999-06-26 20:29:21 +0000

    * tests/windows-matching.scm: Added.
    * tests/cursor.scm: Added

---
+++ scheme/tests/windows-matching.scm
@@ -0,0 +1,18 @@
+;; $Id$
+
+(use-modules (app scwm gtk)
+	     (gtk gtk)
+	     (app scwm string-prompt)
+	     (app scwm winlist)
+
+(define (popup-window-list-matching-string str)
+  "Popup a window list of all windows with titles that match string STR.
+Matching is done with wildcards including \"*\" and \"?\"."
+  (show-window-list-menu #:only (title-match?? str) #:warp-to-first #t))
+
+(define-public (prompt-winlist-by-name)
+  (string-prompt "Windows matching? " popup-window-list-matching-string #:warp #t))
+
+;; (bind-key 'all "C-S-M-w" (thunk prompt-winlist-by-name))
+
+
