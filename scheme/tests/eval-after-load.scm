b38a37c093d4caaeb114d801fe690db6552a4491
Greg J. Badros <gjbadros@sourceforge.net>
1999-07-04 02:51:08 +0000

    Added

---
+++ scheme/tests/eval-after-load.scm
@@ -0,0 +1,17 @@
+;; $Id$
+;; testing code in minimal.scm --07/03/99 gjb
+
+;; (set! *scwm-modules* '())
+;;(cdr (assoc '(app scwm face) *scwm-modules*))
+;; (use-scwm-module-note-success '(app scwm face))
+;; (use-scwm-module-note-success '(app scwm foo))
+;; (use-scwm-module-note-success '(app scwm baz))
+;; (use-scwm-module-note-success '(app scwm bong))
+;; (scwm-module-loaded? '(app scwm bar))
+;; (scwm-module-loaded? '(app scwm face))
+;; (scwm-module-loaded? '(app scwm foo))
+;; (scwm-module-loaded? '(app scwm baz))
+;; (scwm-module-loaded? '(app scwm bong))
+;; *scwm-modules*
+;; (eval-after-load '(app scwm baz) (lambda () (display "loaded baz\n")))
+;; (eval-after-load '(app scwm bong) (lambda () (display "loaded bong\n")))
