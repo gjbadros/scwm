ea796e83792f2dcba023ae00f05cb6d2121469f8
Greg J. Badros <gjbadros@sourceforge.net>
1999-07-05 04:27:03 +0000

    more testing code...

--- scheme/tests/fvwm2-modules.scm
+++ scheme/tests/fvwm2-modules.scm
@@ -1,6 +1,7 @@
-(use-modules (app scwm fvwm-module))
-(use-modules (app scwm module-types))
-(use-modules (app scwm bincomm))
+;; $Id$
+
+(use-scwm-modules fvwm-module module-types bincomm fvwm-compat fvwm-eval)
+
 (fvwm2-module-send-window-list (current-output-port))
 
 (define port (current-output-port))
@@ -16,3 +17,20 @@
 (current-error-port)
 
 (array? "foo")
+
+
+(eval-fvwm-command "Send_WindowList" )
+
+(define fvwm2-module-send-window-list ((@ app scwm fvwm-module) 'fvwm2-module-send-window-list))
+(define fvwm2-module-send-packet ((@ app scwm fvwm-module) 'fvwm2-module-send-packet))
+(define longs->string ((@ app scwm fvwm-module) 'longs->string))
+
+(fvwm2-module-send-window-list (current-output-port))
+
+(uniform-array-write (make-uniform-array (array-prototype "str")) (current-output-port))
+
+(make-uniform-array (array-prototype "str"))
+
+(binary-write "foo")
+
+M_NEW_PAGE
