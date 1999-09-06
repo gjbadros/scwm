54fce24d0ee0b53fa90a7712d56a16e27cad7160
Greg J. Badros <gjbadros@sourceforge.net>
1999-09-06 04:34:05 +0000

    * scwmcorba.c: Added, and moved everything into modules/corba since now the servant works from inside Scwm!

---
+++ modules/corba/scwm_scheme_evaluator-impl.c
@@ -0,0 +1,134 @@
+/* $Id$
+ * (C) 1999 By Greg J. Badros
+ * This is just a dummy, but the CORBA bit seems to be working --09/05/99 gjb
+ */
+
+#include <stdio.h>
+#include "guile-compat.h"
+
+#include "scwm_scheme_evaluator.h"
+
+/*** App-specific servant structures ***/
+typedef struct {
+  POA_scwm_scheme_evaluator_object servant;
+  PortableServer_POA poa;
+
+} impl_POA_scwm_scheme_evaluator_object;
+
+
+
+
+/*** Implementation stub prototypes ***/
+static void impl_scwm_scheme_evaluator_object__destroy(impl_POA_scwm_scheme_evaluator_object *servant,
+                                                       CORBA_Environment *ev);
+static void
+impl_scwm_scheme_evaluator_object_evaluate_sexp(impl_POA_scwm_scheme_evaluator_object *servant,
+                                                CORBA_char * expr,
+                                                CORBA_char ** answer,
+                                                CORBA_char ** output,
+                                                CORBA_char ** error_output,
+                                                CORBA_Environment *ev);
+
+
+
+
+/*** epv structures ***/
+static PortableServer_ServantBase__epv impl_scwm_scheme_evaluator_object_base_epv = {
+  NULL, /* _private data */
+  NULL, /* finalize routine */
+  NULL, /* default_POA routine */
+};
+static POA_scwm_scheme_evaluator_object__epv impl_scwm_scheme_evaluator_object_epv = {
+  NULL, /* _private */
+  (gpointer)&impl_scwm_scheme_evaluator_object_evaluate_sexp,
+
+};
+
+
+/*** vepv structures ***/
+static POA_scwm_scheme_evaluator_object__vepv impl_scwm_scheme_evaluator_object_vepv = {
+  &impl_scwm_scheme_evaluator_object_base_epv,
+  &impl_scwm_scheme_evaluator_object_epv,
+};
+
+
+/*** Stub implementations ***/
+static scwm_scheme_evaluator_object impl_scwm_scheme_evaluator_object__create(PortableServer_POA poa, CORBA_Environment *ev)
+{
+  scwm_scheme_evaluator_object retval;
+  impl_POA_scwm_scheme_evaluator_object *newservant;
+  PortableServer_ObjectId *objid;
+
+  newservant = g_new0(impl_POA_scwm_scheme_evaluator_object, 1);
+  newservant->servant.vepv = &impl_scwm_scheme_evaluator_object_vepv;
+  newservant->poa = poa;
+  POA_scwm_scheme_evaluator_object__init((PortableServer_Servant)newservant, ev);
+  objid = PortableServer_POA_activate_object(poa, newservant, ev);
+  CORBA_free(objid);
+  retval = PortableServer_POA_servant_to_reference(poa, newservant, ev);
+
+  return retval;
+}
+
+static void
+impl_scwm_scheme_evaluator_object__destroy(impl_POA_scwm_scheme_evaluator_object *servant, CORBA_Environment *ev)
+{
+  PortableServer_ObjectId *objid;
+
+  objid = PortableServer_POA_servant_to_id(servant->poa, servant, ev);
+  PortableServer_POA_deactivate_object(servant->poa, objid, ev);
+  CORBA_free(objid);
+
+  POA_scwm_scheme_evaluator_object__fini((PortableServer_Servant)servant, ev);
+  g_free(servant);
+}
+
+static void
+impl_scwm_scheme_evaluator_object_evaluate_sexp(impl_POA_scwm_scheme_evaluator_object *servant,
+                                                CORBA_char * expr,
+                                                CORBA_char ** answer,
+                                                CORBA_char ** output,
+                                                CORBA_char ** error_output,
+                                                CORBA_Environment *ev)
+{
+#define FUNC_NAME "impl_scwm_scheme_evaluator_object_evaluate_sexp"
+  SCM val, str_val;
+  SCM o_port, e_port;
+  SCM saved_def_e_port;
+  char *szAnswer;
+  char *szOutput;
+  char *szErrorOutput;
+  
+  /* Temporarily redirect output and error to string ports. 
+     Note that the port setting functions return the current previous
+     port. */
+  o_port = scm_set_current_output_port(make_output_strport(FUNC_NAME));
+  e_port = scm_set_current_error_port(make_output_strport(FUNC_NAME));
+  
+  /* Workaround for a problem with older Guiles */
+  saved_def_e_port = scm_def_errp;
+  scm_def_errp = scm_current_error_port();
+  
+  /* Evaluate the request expression and free it. */
+  val = scwm_safe_eval_str(expr);
+  str_val = scm_strprint_obj(val);
+  szAnswer = gh_scm2newstr(str_val, NULL);
+  
+  /* restore output and error ports; use returned o_port/e_port
+     below for getting the strings back */
+  o_port = scm_set_current_output_port(o_port);
+  e_port = scm_set_current_error_port(e_port);
+  scm_def_errp = saved_def_e_port;
+  
+  /* Retrieve output and errors */
+  szOutput = gh_scm2newstr(scm_strport_to_string(o_port),NULL);
+  szErrorOutput = gh_scm2newstr(scm_strport_to_string(e_port),NULL);
+          
+  *answer = CORBA_string_dup(szAnswer);
+  *output = CORBA_string_dup(szOutput);
+  *error_output = CORBA_string_dup(szErrorOutput);
+  gh_free(szAnswer);
+  gh_free(szOutput);
+  gh_free(szErrorOutput);
+}
+#undef FUNC_NAME
---
+++ modules/corba/scwmcorba.c
@@ -0,0 +1,140 @@
+/* $Id$
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <orb/orbit.h>
+#include <libgnorba/gnorba.h>
+#include <gtk/gtk.h>
+#include <guile/gh.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "scwm_msg.h"
+#include "scwm-snarf.h"
+#include "callbacks.h"
+
+#include "scwm_scheme_evaluator.h"
+#include "scwm_scheme_evaluator-impl.c"
+
+/* decleration to avoid compiler error.  Many of these functions would be 
+ * in a header in a normal application. */
+void scwm_scheme_evaluator_corba_gtk_init(int argc, char *argv[]);
+
+static CORBA_ORB orb = NULL;
+static CORBA_Environment ev;
+static scwm_scheme_evaluator_object evaluator_object;
+static SCM scmIOR;
+
+/* Shutdown the orb interface, and shut down GTK */
+void
+scwm_scheme_evaluator_corba_gtk_main_quit(void)
+{
+  CORBA_ORB_shutdown(orb, CORBA_FALSE, &ev);
+  gtk_main_quit();
+}
+
+
+#define NAME "scwm-evaluator"
+
+void Exception( CORBA_Environment* ev )
+{
+  switch( ev->_major ) {
+  case CORBA_SYSTEM_EXCEPTION:
+    g_log(NAME, G_LOG_LEVEL_DEBUG, "CORBA system exception %s.\n",
+          CORBA_exception_id(ev));
+    break;
+  case CORBA_USER_EXCEPTION:
+    g_log(NAME, G_LOG_LEVEL_DEBUG, "CORBA user exception: %s.\n",
+          CORBA_exception_id( ev ) );
+    break;
+  default:
+    break;
+  }
+}
+
+
+/* initialize the gtk handling of the corba services */
+void
+scwm_scheme_evaluator_corba_gtk_init(int argc, char *argv[])
+{
+  PortableServer_POA          root_poa;
+  PortableServer_POAManager   pm;
+  gchar *ior;
+
+  CORBA_exception_init(&ev);
+
+  orb = gnome_CORBA_init(NAME,VERSION,&argc, argv, 
+                         GNORBA_INIT_SERVER_FUNC, &ev);
+
+  root_poa = (PortableServer_POA) 
+    CORBA_ORB_resolve_initial_references(orb, "RootPOA", &ev);
+  Exception(&ev);
+  
+  evaluator_object = impl_scwm_scheme_evaluator_object__create(root_poa, &ev);
+  Exception(&ev);
+  
+  pm = PortableServer_POA__get_the_POAManager(root_poa, &ev);
+  Exception(&ev);
+  
+  PortableServer_POAManager_activate(pm, &ev);
+  Exception(&ev);
+  
+  goad_server_register(NULL, evaluator_object, "scwm-scheme-evaluator", "object", &ev);
+  
+  ior = CORBA_ORB_object_to_string(orb, evaluator_object , &ev);
+
+  gh_defer_ints();
+  scmIOR = gh_str02scm(ior);
+  scm_permanent_object(scmIOR);
+  gh_allow_ints();
+    
+  ORBit_custom_run_setup(orb, &ev);
+}
+
+/* This is the beginning of the GTK interface.  In a larger project,
+ * this would likely be in a seperate file. 
+ * Note that I'm not going to comment this part, as I'll assume you know
+ * how GTK works */
+
+gint delete_event(GtkWidget *widget, GdkEvent *event, gpointer data)
+{
+  scwm_scheme_evaluator_corba_gtk_main_quit();
+  return (FALSE);
+}
+
+
+SCWM_PROC(corba_evaluator_ior, "corba-evaluator-ior", 0, 0, 0,
+          ())
+     /** Returns the IOR string for the scwm-scheme-evaluator servant. */
+#define FUNC_NAME s_corba_evaluator_ior
+{
+  return scmIOR;
+}
+#undef FUNC_NAME
+
+
+static void
+init_scwmcorba()
+{
+  int argc = 1;
+  char *argv[1] = { NAME };
+  
+  scwm_scheme_evaluator_corba_gtk_init(argc,argv);
+
+#ifndef SCM_MAGIC_SNARFER
+ #include "scwmcorba.x"
+#endif
+}
+
+void scm_init_app_scwm_scwmcorba_module()
+{
+  scm_register_module_xxx("app scwm scwmcorba", init_scwmcorba);
+}
+
+/* Local Variables: */
+/* tab-width: 8 */
+/* c-basic-offset: 2 */
+/* End: */
