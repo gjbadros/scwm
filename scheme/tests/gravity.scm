8907b88e812609fa3daea638ca51ae845d104243
Greg J. Badros <gjbadros@sourceforge.net>
1999-10-01 00:20:10 +0000

    More complete tests

--- scheme/tests/gravity.scm
+++ scheme/tests/gravity.scm
@@ -1,12 +1,62 @@
+#!
+!#
+
+
 (window-gravity (car a))
 (window-gravity (get-window))
 
-(define w1 (get-window))
-(define w2 (get-window))
+(begin
+  (define w1 (get-window))
+  (define w2 (get-window))
+  (define w3 (get-window))
+  (list (window-gravity w1)
+	(window-gravity w2)
+	(window-gravity w3))
+  )
+
+(resize-frame 100 100 w1)
+(resize-frame 200 200 w1)
+(resize-frame 100 100 w2)
+(resize-frame 200 200 w2)
+(resize-frame 100 100 w3)
+(resize-frame 200 200 w3)
+
+(use-scwm-modules animation)
+
+(animated-resize-frame 100 100 w1)
+(animated-resize-frame 200 200 w1)
+(animated-resize-frame 100 100 w2)
+(animated-resize-frame 200 200 w2)
+(animated-resize-frame 100 100 w3)
+(animated-resize-frame 200 200 w3)
+
+
+
+(resize-frame 100 100 w1 0 0)
+(resize-frame 200 200 w1 0 0)
+(resize-frame 100 100 w2 0 0)
+(resize-frame 200 200 w2 0 0)
+(resize-frame 100 100 w3 0 0)
+(resize-frame 200 200 w3 0 0)
+
+(use-scwm-modules animation)
 
 (animated-resize-frame 100 100 w1 0 0)
+(animated-resize-frame 200 200 w1 0 0)
 (animated-resize-frame 100 100 w2 0 0)
+(animated-resize-frame 200 200 w2 0 0)
+(animated-resize-frame 100 100 w3 0 0)
+(animated-resize-frame 200 200 w3 0 0)
+
+
+(define* (little-top-left #&optional (win (get-window)))
+  (animated-resize-frame 100 100 win 0 0))
+
+(bind-key 'window "H-a" little-top-left)
+(animated-resize-frame 100 100 w2 0 0)
+(animated-resize-frame 100 100 w3 0 0)
 (resize-frame 100 100 w2 0 0)
 
 (resize-frame 100 100 w1)
 (resize-frame 100 100 w1)
+
