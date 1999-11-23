c483222ce8bd6ed5bfc6876573c001cfbc1ab6dc
Greg J. Badros <gjbadros@sourceforge.net>
1999-11-23 20:22:21 +0000

    * visibility-reporter.scm: Added to demonstrate visibility hooks.

---
+++ scheme/visibility-reporter.scm
@@ -0,0 +1,19 @@
+;;; $Id$
+;;; (C) 1999 Greg J. Badros
+;;; See also the `window-visibility' primitive
+
+(define-module (app scwm visibility-reporter))
+
+(define (report-unobscured win) (display "unobscured") (write win) (newline))
+(define (report-partially-obscured win) (display "partially-obscured") (write win) (newline))
+(define (report-fully-obscured win) (display "fully-obscured") (write win) (newline))
+
+(define-public (install-visibility-reporter)
+  (add-hook! window-unobscured-hook report-unobscured)
+  (add-hook! window-fully-obscured-hook report-fully-obscured)
+  (add-hook! window-partially-obscured-hook report-partially-obscured))
+
+(define-public (uninstall-visibility-reporter)
+  (remove-hook! window-unobscured-hook report-unobscured)
+  (remove-hook! window-fully-obscured-hook report-fully-obscured)
+  (remove-hook! window-partially-obscured-hook report-partially-obscured))
