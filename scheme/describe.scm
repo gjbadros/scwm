9e02a0f636396094d2441c00fd059fde92859098
Greg J. Badros <gjbadros@sourceforge.net>
2000-01-09 01:50:39 +0000

    * describe.scm: Added `describe-event', and factor out `display-binding' from all the `describe-*' procedures.  Also export `is-mouse-event?'

--- scheme/describe.scm
+++ scheme/describe.scm
@@ -7,28 +7,41 @@
   :use-module (app scwm message-window)
   :use-module (app scwm reflection))
 
+(define-public (is-mouse-event? event)
+  "Return #t iff EVENT is a mouse event.
+EVENT should be a list returned by `get-next-event',
+`get-key-event', or `get-mouse-event'."
+  (and event (eqv? (length event) 4)))
+
+(define (display-binding key procs)
+  (display-message-briefly
+   (string-append key " is bound to " 
+		  (if (pair? procs)
+		      (string-append (procedure->string (car procs))
+				     ", "
+				     (procedure->string (cadr procs)))
+		      "nothing"))))
+
+
+;;; GJB:FIXME:: handle bindings that are not for the 'all context
+(define*-public (describe-event)
+  "Display a message giving the bindings of the next event, key or mouse."
+  (interactive)
+  (let* ((event (get-next-event))
+	 (key (car event))
+	 (procs ((if (is-mouse-event? event) lookup-mouse lookup-key) 'all key)))
+    (display-binding key procs)))
+
 (define*-public (describe-key)
   "Display a message giving the bindings of the next key event."
   (interactive)
   (let* ((key (car (get-key-event)))
 	 (procs (lookup-key 'all key)))
-    (display-message-briefly
-     (string-append key " is bound to " 
-		    (if (pair? procs)
-			(string-append (procedure->string (car procs))
-				       ", "
-				       (procedure->string (cadr procs)))
-			"nothing")))))
+    (display-binding key procs)))
 
 (define*-public (describe-mouse)
   "Display a message giving the bindings of the next mouse event."
   (interactive)
   (let* ((key (car (get-mouse-event)))
 	 (procs (lookup-mouse 'all key)))
-    (display-message-briefly
-     (string-append key " is bound to " 
-		    (if (pair? procs)
-			(string-append (procedure->string (car procs))
-				       ", "
-				       (procedure->string (cadr procs)))
-			"nothing")))))
+    (display-binding key procs)))
