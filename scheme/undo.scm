09210dd62b6433a2635ef9a3f40f78a5bbf2b980
Scott Lenser <slenser@sourceforge.net>
2002-03-30 08:31:41 +0000

    * undo.scm: Documented public variables. Updated and greatly improved documentation. Deprecated 'decrement-undo-index', 'increment-undo-index', 'insert-undo-global', and 'insert-undo-state'.  The first 2 should be private and the last 2 were broken and had bizarre semantics to begin with so I decided to deprecate instead of fix.
    Fixed bug in undo/redo that would result in having to do 2
    undos/redos in a row to reverse the effect of the last redo/undo.
    
    Implemented paying attention to undo-max-entries so that we don't
    just keep growing memory in the comon usage case.  Added function
    'truncate-undo-list' in the process to constrain the undo-list to
    be no larger than a given number.  Exported as public since may be
    useful to users.

--- scheme/undo.scm
+++ scheme/undo.scm
@@ -25,10 +25,18 @@
   :use-module (app scwm window-configuration)
   :use-module (app scwm winlist))
 
+;;; The list of undo states
 (define-public undo-list '())
+;;; The number of elements in undo-list (i.e. (length undo-list))
 (define-public undo-num-entries 0)
+;;; The maximum number of undo entries to keep.
 (define-public undo-max-entries 100)
+;;; The index in undo-list of the state to copy on the next undo
 (define-public undo-index 0)
+
+;;; SRL:FIXME:: Change so it doesn't use negative logic as double negatives are
+;;;   confusing.
+(define last-not-redo #t)
 
 (define*-public (reset-undo!)
   "Reset the undo system.
@@ -36,49 +44,74 @@ Clears the list of states."
   (interactive)
   (set! undo-list '())
   (set! undo-num-entries 0)
-  (set! undo-index 0))
+  (set! undo-index 0)
+  (set! last-not-redo #t))
 
-;; high is inside range
+(define-public (truncate-undo-list num-entries)
+  "Truncate 'undo-list' such that it has no more than NUM-ENTRIES."
+  (let ((extra-entries (- undo-num-entries num-entries)))
+    (if (> extra-entries 0)
+        (begin 
+          (if (> num-entries 0)
+              (set-cdr! (list-tail undo-list (- num-entries 1)) '())
+              (set! undo-list '()))
+          (set! undo-num-entries num-entries)))))
+
+;;; high is inside range
 (define-public (increment-in-range v high)
   "Return v+1 but no more than high."
   (set! v (+ v 1))
-  (if (> v high) (- high 1) v))
+  (if (> v high) high v))
 
-;; low is inside range
+;;; low is inside range
 (define-public (decrement-in-range v low)
   "Return v-1 but no less than low."
   (set! v (- v 1))
   (if (< v low) low v))
 
 (define-public (decrement-undo-index)
-  "Decrement the undo-index."
-  (set! undo-index (decrement-in-range undo-index 0)))
+  "DEPRECATED: Decrement the undo-index.
+Will become a private function."
+  (set! undo-index (decrement-in-range undo-index 1)))
 
 (define-public (increment-undo-index)
-  "Increment the undo-index."
+  "DEPRECATED: Increment the undo-index.
+Will become a private function."
   (set! undo-index (increment-in-range undo-index undo-num-entries)))
 
 (define*-public (push-undo-global)
   "Push the global state onto the undo list.
-See also `insert-undo-global' which honours
-the current position in the undo-list, undo-index."
+See also 'push-undo-state' and 'global-window-configuration'."
   (interactive)
   (push-undo-state (global-window-configuration)))
 
 (define*-public (insert-undo-global)
-  "Insert the global state into the undo list at undo-index.
-See also `push-undo-global'."
+  "DPRECATED: BROKEN: Same as 'push-undo-global'.
+Was documented as: Insert the global state into the undo list at undo-index.
+This function does not work as previously documented and has
+bizarre semantics which seem useless and therefor will not be
+fixed and is deprecated."
   (interactive)
   (insert-undo-state (global-window-configuration)))
 
+;;; SRL:FIXME:: This shouldn't assume that global window configurations
+;;;   have this particular format.
 (define-public (push-undo-state state)
-  "Push STATE onto the undo list."
+  "Push STATE onto the undo list.
+STATE should be a list of window configurations to save in stacking order
+from top to bottom.  See also 'push-undo-global'."
   (set! undo-list (cons state undo-list))
   (set! undo-num-entries (+ undo-num-entries 1))
-  (set! undo-index 0))
+  (truncate-undo-list undo-max-entries)
+  (set! undo-index 0)
+  (set! last-not-redo #t))
 
 (define-public (insert-undo-state state)
-  "Insert STATE into the undo list at undo-index."
+  "DEPRECATED: BROKEN: Same as 'push-undo-state'
+Was documented as:Insert STATE into the undo list at undo-index.
+This function does not work as previously documented and has
+bizarre semantics which seem useless and therefor will not be
+fixed and is deprecated."
   (if (<= 0 undo-index)
       (push-undo-state state)
       (begin
@@ -87,17 +120,19 @@ See also `push-undo-global'."
 	(set! undo-num-entries (+ undo-num-entries 1)))))
 
 (define-public (undo-state-at-index index)
-  "Return the state from undo-list at INDEX, or #f if bad index."
+  "Return the state from undo-list at INDEX, or #f if bad index.
+INDEX of 0 means the state before the last undoable action (i.e. the state
+as of the last time push-undo-global was called).  Each additonal 1 added
+to INDEX goes back one more undoable operation.  See also 'undo-use'."
   (if (and (>= index 0) (< index undo-num-entries))
       (list-ref undo-list index)
       #f))
 
-(define last-not-redo #t)
-
 (define*-public (undo)
   "Undo the last operation that was undoable.
-Undoable operations save changed state using `insert-undo-global'
-before they perform their action."
+Undoable operations save changed state using `push-undo-global'
+before they perform their action.  Undo is an undoable action
+if 'undo' or 'push-undo-state' was called since the last 'redo'."
   (interactive)
   (if (and last-not-redo (= 0 undo-index))
       (begin (push-undo-global)
@@ -110,20 +145,21 @@ before they perform their action."
   undo-index)
 
 (define*-public (redo)
-  "Redo the last undone operation.
-This re-applyiesthe state at undo-index - 1."
+  "Redo the last undone operation."
   (interactive)
-  (if (= undo-num-entries undo-index)
-      (decrement-undo-index))  ;; do not redo the same state we just undid
   (set! last-not-redo #f)
   (decrement-undo-index)
-  (let ((state (undo-state-at-index undo-index)))
+  (let ((state (undo-state-at-index (- undo-index 1))))
     (if state
 	(restore-global-window-configuration state)))
   undo-index)
 
 (define-public (undo-use index)
-  "Apply the state from the undo-list contained at INDEX."
+  "Apply the state from the undo-list contained at INDEX.
+INDEX of 0 means the state before the last undoable action (i.e. the state
+as of the last time push-undo-global was called).  Each additonal 1 added
+to INDEX goes back one more undoable operation.  See also
+'undo-state-at-index'."
   (let ((state (undo-state-at-index index)))
     (if state
 	(restore-global-window-configuration state)))
@@ -131,7 +167,6 @@ This re-applyiesthe state at undo-index - 1."
 
 ;; (reset-undo!)
 ;; (push-undo-global)
-;; (insert-undo-global)
 ;; (undo)
 ;; (redo)
 ;; undo-num-entries
