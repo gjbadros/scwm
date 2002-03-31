ba1d8607d14b58770d5259543627e5edcb71083d
Scott Lenser <slenser@sourceforge.net>
2002-03-31 22:50:44 +0000

    * virtual.scm: Various doc fixes/improvements. Added FIXMEs about changing names to be consistent. Added FIXME about strange location choice by move-to-viewport. Added FIXME about dubious assumptions (that currently hold) in   'deiconify-to-current-viewport'. Deprecated 'switch-to-first-desk' and 'switch-to-second-desk'   since they are way to specific and have names longer than the function calls they replace. Added 'switch-to-home-desk' as reasonably special enough to have   its own function.  This was names 'switch-to-first-desk'.

--- scheme/virtual.scm
+++ scheme/virtual.scm
@@ -32,13 +32,13 @@
 ;;;
 
 (define-public (containing-aligned-viewport pos)
-  "Compute the viewport-position of the viewport that contains POS.
-POS is a list of x and y virtual coordinates in that order. The
-viewport coordinates returned will be in the same format and will be a
-integral multiples of the desk size. If POS is not contained in any
-valid viewport, for instance if it includes coordinates greater than
-zero or greater than the desk size times the display size, the nearest
-viewport will be returned instead."
+  "Compute the viewport-position of the aligned viewport that contains POS.
+POS is a virtual coordinate represented as a list of x,y in that order. The
+viewport coordinates returned will be in the same format and will be an
+integral multiple of the desk size. If POS is not contained in any valid
+viewport, for instance if it includes coordinates less than zero or
+greater than the desk size times the display size, the coordinates
+of the nearest viewport will be returned instead."
   (map (lambda (vc ds dds)
 	 (max 0 (min (* (- dds 1) ds) (- vc (modulo vc ds)))))
        pos (display-size) (desk-size)))
@@ -46,9 +46,9 @@ viewport will be returned instead."
 
 (define-public (nearest-aligned-viewport pos)
   "Compute the nearest valid integral viewport position to POS.
-POS is a list of x and y virtual coordinates in that order. The
-viewport coordinates returned will be in the same format and will be a
-integral multiples of the desk size."
+POS is a virtual coordinate represented as a list of x,y in that order. The
+viewport coordinates returned will be in the same format and will be an
+integral multiple of the desk size."
   (map (lambda (vc ds dds)
 	 (max 0 (min (* ds (- dds 1)) 
 		     (let ((mvcds (modulo vc ds)))
@@ -58,7 +58,7 @@ integral multiples of the desk size."
        pos (display-size) (desk-size)))
 
 (define-public (window-aligned-viewport win)
-  "Return the viewport that contains the center of WIN.
+  "Return the viewport-positon of the viewport that contains the center of WIN.
 The return value is an integral multiple of the viewport size which
 contains the center of the window, or the closest possible if none
 does."
@@ -84,7 +84,7 @@ The returned value will be a non-negative real number."
 The (0,0) viewport is the starting viewport.  XX and YY are
 full display-size increments (e.g., (1,0) is the viewport
 just to the right of the home (0,0) viewport).  This returns
-the position that `move-window-to-viewport' moves the window to."
+the position that `move-window-to-viewport' would move the window to."
   (let ((d-s (desk-size)))
     (if (or (> xx (- (car d-s) 1)) (> yy (- (cadr d-s) 1)))
 	(error "viewport position outside range of desk-size")))
@@ -92,6 +92,8 @@ the position that `move-window-to-viewport' moves the window to."
 	 (w-f-s (window-frame-size win))
 	 (width (car w-f-s))
 	 (height (cadr w-f-s)))
+    ;; SRL:FIXME:: Can't we avoid unnecessarily changing the windows
+    ;;   position relative to the viewport?
     (list (+ (* xx display-width)
 	     (modulo (car pos) (- display-width (round/ width 2))))
 	  (+ (* yy display-height)
@@ -107,8 +109,9 @@ just to the right of the home (0,0) viewport).  Uses
 the viewport."
   (apply move-window (append (window-position-in-viewport xx yy win) (list win))))
 
+;; SRL:FIXME:: Argument order different than all the function above. *sigh*
 (define-public (move-inside-viewport win x y)
-  "Ensure that WIN is entirely inside the X, Y viewport if possible.
+  "Ensure that WIN is entirely inside the viewport at X, Y if possible.
 X and Y are given in pixels."
   (let ((final-position (map (lambda (wp vp ds ws)
 			       (cond 
@@ -119,17 +122,20 @@ X and Y are given in pixels."
 			     (display-size) (window-frame-size win))))
     (move-window (car final-position) (cadr final-position) win)))
 
+;; SRL:FIXME:: Argument order different than all the function above. *sigh*
 (define-public (move-inside-aligned-viewport win xx yy)
   "Ensure that WIN is entirely inside the XX, YY viewport if possible.
-XX and YY are given in units of the display size."
+XX and YY are given in units of the display size (e.g. (1,0) is the
+viewport just to the right of the home (0,0) viewport)."
   (apply move-inside-viewport win (map * (list xx yy) (display-size))))
 
 (define-public (move-inside-current-viewport win)
   "Ensure that WIN is entirely inside the current viewport, if possible."
   (apply move-inside-viewport win (viewport-position)))
 
+;; SRL:FIXME:: Add aligned to name for consistency.
 (define-public (move-inside-own-viewport win)
-  "Ensure that WIN is entirely inside the closest viewport to its center.
+  "Ensure WIN is entirely inside the aligned viewport containing its center.
 The viewport selected will be an integral multiple of the desk size."
   (apply move-inside-viewport win (window-aligned-viewport win)))
 
@@ -137,6 +143,9 @@ The viewport selected will be an integral multiple of the desk size."
   "De-iconify WIN and make it visible in the current viewport."
   (let ((vpx (current-viewport-offset-xx))
 	(vpy (current-viewport-offset-yy)))
+    ;; SRL:FIXME:: This works but makes all kind of dubious assumptions.
+    ;;   Assumes that window-position-in-viewport works with fractional numbers.
+    ;;   Assumes that deiconify-window can accept fractional numbers.
     (apply deiconify-window
 	   (append (list win) (window-position-in-viewport vpx vpy win)))))
 
@@ -144,8 +153,9 @@ The viewport selected will be an integral multiple of the desk size."
 ;;; Higher-level operations for setting the current viewport.
 ;;;
 
+;;; SRL:FIXME:: Add aligned to function name.
 (define-public (move-to-viewport xx yy)
-    "Move to the viewport numbered (XX,YY).
+    "Move to the aligned viewport numbered (XX,YY).
 The (0,0) viewport is the starting viewport.  XX and YY are
 full display-size increments (e.g., (1,0) is the viewport
 just to the right of the home (0,0) viewport)."
@@ -156,8 +166,9 @@ just to the right of the home (0,0) viewport)."
   (apply set-viewport-position! 
 	 (nearest-aligned-viewport (viewport-position))))
 
+;;; SRL:FIXME:: Add aligned to funciton name.
 (define-public (warp-to-window-viewport win)
-  "Change to the desk and viewport of WIN.
+  "Change to the desk and aligned viewport of WIN.
 The viewport selected is an integral multiple of the viewport size
 which contains the center of the window, or the closest possible if
 none does."
@@ -195,12 +206,15 @@ BODY, and restored on each exit, including non-local exits."
 	  (set-current-desk! ,desk)
 	  (apply set-viewport-position! ,vpos))))))
 
-(define*-public (switch-to-first-desk)
-  "Switch to the first desktop."
+(define*-public (switch-to-home-desk)
+  "Switch to the home (first, i.e. 0) desktop."
   (interactive)
   (set-current-desk! 0))
 
+;;; DEPRECATED
+(define-public switch-to-first-desk switch-to-home-desk)
+
 (define*-public (switch-to-second-desk) 
-  "Switch to the second desktop."
+  "DEPRECATED: Switch to the second desktop."
   (interactive)
   (set-current-desk! 1))
