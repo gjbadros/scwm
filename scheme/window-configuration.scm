8a4bb4f8a77bb876b63fd82de93b27dd3ad87053
Scott Lenser <slenser@sourceforge.net>
2002-03-31 01:28:38 +0000

    * window-configuration.scm: Added a FIXME about the desk of windows being saved.

--- scheme/window-configuration.scm
+++ scheme/window-configuration.scm
@@ -33,6 +33,8 @@
 ;;; in several ways.  The window-configuration needs to have the icon position
 ;;; added to it and probably keep on top status.
 
+;;; SRL:FIXME::Should save which desk the window is on.
+
 (define-module (app scwm window-configuration)
   :use-module (app scwm wininfo)
   :use-module (app scwm optargs)
