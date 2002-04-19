6b029f6b022eb5bb603e0502da41bbed3afec0dc
Scott Lenser <slenser@sourceforge.net>
2002-04-19 04:37:48 +0000

    * synth-events.scm: Added commentary to doc noting that the synthetic events sent are ignored by some applications.

--- scheme/synth-events.scm
+++ scheme/synth-events.scm
@@ -5,16 +5,19 @@
   :use-module (app scwm optargs))
 
 (define*-public (send-button-1)
-  "Send a mouse-1 button press and release event."
+  "Send a mouse-1 button press and release event.
+Note: this sends synthetic events which some applications ignore."
   (interactive)
   (send-button 1))
 
 (define*-public (send-button-2)
-  "Send a mouse-2 button press and release event."
+  "Send a mouse-2 button press and release event.
+Note: this sends synthetic events which some applications ignore."
   (interactive)
   (send-button 2))
 
 (define*-public (send-button-3)
-  "Send a mouse-3 button press and release event."
+  "Send a mouse-3 button press and release event.
+Note: this sends synthetic events which some applications ignore."
   (interactive)
   (send-button 3))
